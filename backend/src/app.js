fa68e9ce1908aa8a710e97f8ce06b09a7e4b0f89
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-04 12:04:27 -0300

    configured cors and cookie parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,16 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+
+app.use(cookieParser(process.env.COOKIE_PARSER_SECRET))
+app.use(express.json())
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+)
 
 app.get("/", (req, res) => {
     res.send("Bem-vindo a minha API, para mais informações, confira o repositório do projeto")
