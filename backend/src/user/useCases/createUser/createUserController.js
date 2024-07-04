0e6a0711f0d1b77bd51a76230d0dc123546bfa37
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-04 10:40:34 -0300

    added the error return to the controller

--- backend/src/user/useCases/createUser/createUserController.js
+++ backend/src/user/useCases/createUser/createUserController.js
@@ -1,5 +1,9 @@
 const handle = async(req, res) => {
     const user = await createUserUseCase(req.body)
 
+    if (user.status !== 201) {
+        res.status(user.status).json({ msg: user.msg })
+    }
+
     res.status(200).json({ msg: "Usuario criado com sucesso", user })
 }
