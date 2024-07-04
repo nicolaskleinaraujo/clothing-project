6914fd6981d6e76513f08c1979546216ea0a0256
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-04 10:43:42 -0300

    refactoring the design pattern

--- backend/src/user/useCases/createUser/createUserController.js
+++
@@ -1,9 +0,0 @@
-const handle = async(req, res) => {
-    const user = await createUserUseCase(req.body)
-
-    if (user.status !== 201) {
-        res.status(user.status).json({ msg: user.msg })
-    }
-
-    res.status(200).json({ msg: "Usuario criado com sucesso", user })
-}
--- backend/src/user/useCases/createUser/createUserUseCase.js
+++
@@ -1,8 +0,0 @@
-const prisma = require("../../../db/client")
-
-const execute = async(body) => {
-    const emailExists = prisma.user.findUnique({ where: { email: body.email } })
-    if (emailExists) {
-        
-    }
-}
