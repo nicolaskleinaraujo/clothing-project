b491042439405f47d9c074932ef107b8769fd924
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-04 11:10:31 -0300

    added bcrypt and jwt to the backend

--- backend/src/controllers/user/createUser/createUserController.js
+++ backend/src/controllers/user/createUser/createUserController.js
@@ -1,44 +1,46 @@
 const prisma = require("../../../db/client")
 
-const createUserController = {
-    execute: async (req, res) => {
-        const {
-            firstName,
-            lastName,
-            email,
-            number,
-            password,
-            cep,
-            city,
-            district,
-            street,
-            houseNum,
-        } = req.body
+const createUserController = async (req, res) => {
+    const {
+        firstName,
+        lastName,
+        email,
+        number,
+        password,
+        cep,
+        city,
+        district,
+        street,
+        houseNum,
+    } = req.body
 
-        parseInt(houseNum)
+    parseInt(houseNum)
 
-        if (
-            firstName === "" ||
-            lastName === "" ||
-            email === "" ||
-            number === "" ||
-            password === "" ||
-            cep === "" ||
-            city === "" ||
-            district === "" ||
-            street === "" ||
-            houseNum === undefined
-        ) {
-            res.status(400).json({ msg: "Informações insuficientes" })
-            return
-        }
-
-        const emailExists = await prisma.user.findUnique({ where: { email } })
-        const numberExists = await prisma.user.findUnique({ where: { number } })
-
-        if (emailExists || numberExists) {
-            res.status(400).json({ msg: "Email ou numero de telefone já cadastrado" })
-            return
-        }
+    if (
+        firstName === "" ||
+        lastName === "" ||
+        email === "" ||
+        number === "" ||
+        password === "" ||
+        cep === "" ||
+        city === "" ||
+        district === "" ||
+        street === "" ||
+        houseNum === undefined
+    ) {
+        res.status(400).json({ msg: "Informações insuficientes" })
+        return
     }
+
+    const emailExists = await prisma.user.findUnique({ where: { email } })
+    const numberExists = await prisma.user.findUnique({ where: { number } })
+
+    if (emailExists || numberExists) {
+        res.status(400).json({ msg: "Email ou numero de telefone já cadastrado" })
+        return
+    }
+
+    
 }
+
+module.exports = createUserController
