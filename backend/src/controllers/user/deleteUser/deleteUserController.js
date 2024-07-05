63f8814d68e312efa4fc2219c8698bb64c66e7d1
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-05 12:13:02 -0300

    created the deleteUser controller

---
+++ backend/src/controllers/user/deleteUser/deleteUserController.js
@@ -0,0 +1,9 @@
+const prisma = require("../../../db/client")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+const deleteUserController = async (req, res) => {
+    
+}
+
+module.exports = deleteUserController
