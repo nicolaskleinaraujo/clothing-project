7da9774c84ea7f496d9a60544fa87b1e32969eaf
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-05 11:38:27 -0300

    created the update user controller

---
+++ backend/src/controllers/user/updateUser/updateUserController.js
@@ -0,0 +1,9 @@
+const prisma = require("../../../db/client")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+const updateUserController = async (req, res) => {
+    
+}
+
+module.exports = updateUserController
