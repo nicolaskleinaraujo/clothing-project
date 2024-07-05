b09ea645d4e41541ef2f7d71c6c00301b75f3499
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-05 11:42:05 -0300

    added the updateUser to the router

--- backend/src/controllers/user/index.js
+++ backend/src/controllers/user/index.js
@@ -1,7 +1,11 @@
+// Controllers
 const createUserController = require("./createUser/createUserController")
+const updateUserController = require("./updateUser/updateUserController")
 
+// Exporting all controllers
 const userController = {
     createUserController,
+    updateUserController,
 }
 
 module.exports = userController
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -4,5 +4,6 @@ const userController = require("../controllers/user/index")
 
 // Routes
 router.route("/").post((req, res) => userController.createUserController(req, res))
+router.route("/").put((req, res) => userController.updateUserController(req, res))
 
 module.exports = router
