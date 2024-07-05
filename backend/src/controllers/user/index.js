a7121c2e554c32f2588329fa92d03613112b00a2
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-05 12:13:53 -0300

    added deleteUser controller to the router

--- backend/src/controllers/user/index.js
+++ backend/src/controllers/user/index.js
@@ -1,11 +1,13 @@
 // Controllers
 const createUserController = require("./createUser/createUserController")
+const deleteUserController = require("./deleteUser/deleteUserController")
 const updateUserController = require("./updateUser/updateUserController")
 
 // Exporting all controllers
 const userController = {
     createUserController,
     updateUserController,
+    deleteUserController,
 }
 
 module.exports = userController
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -5,5 +5,6 @@ const userController = require("../controllers/user/index")
 // Routes
 router.route("/").post((req, res) => userController.createUserController(req, res))
 router.route("/").put((req, res) => userController.updateUserController(req, res))
+router.route("/").delete((req, res) => userController.deleteUserController(req, res))
 
 module.exports = router
