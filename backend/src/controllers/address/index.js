9dfcd2239d7036a9d1094ded26fdfd9433abbe2d
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-06 13:12:55 -0300

    added the delete address to the router

--- backend/src/controllers/address/index.js
+++ backend/src/controllers/address/index.js
@@ -1,11 +1,13 @@
 // Controllers
 const createAddressController = require("./createAddress/createAddressController")
+const deleteAddressController = require("./deleteAddress/deleteAddressController")
 const updateAddressController = require("./updateAddress/updateAddressController")
 
 // Exporting all controllers
 const userController = {
     createAddressController,
     updateAddressController,
+    deleteAddressController,
 }
 
 module.exports = userController
--- backend/src/routes/address.js
+++ backend/src/routes/address.js
@@ -5,5 +5,6 @@ const addressController = require("../controllers/address/index")
 // Routes
 router.route("/").post((req, res) => addressController.createAddressController(req, res))
 router.route("/").put((req, res) => addressController.updateAddressController(req, res))
+router.route("/").delete((req, res) => addressController.deleteAddressController(req, res))
 
 module.exports = router
