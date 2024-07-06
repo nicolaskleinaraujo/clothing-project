d8f8d0f4d588c606cec89e278bfc4da90251fd37
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-06 12:43:18 -0300

    added the update address to the router

--- backend/src/controllers/address/index.js
+++ backend/src/controllers/address/index.js
@@ -1,9 +1,11 @@
 // Controllers
 const createAddressController = require("./createAddress/createAddressController")
+const updateAddressController = require("./updateAddress/updateAddressController")
 
 // Exporting all controllers
 const userController = {
     createAddressController,
+    updateAddressController,
 }
 
 module.exports = userController
--- backend/src/routes/address.js
+++ backend/src/routes/address.js
@@ -4,5 +4,6 @@ const addressController = require("../controllers/address/index")
 
 // Routes
 router.route("/").post((req, res) => addressController.createAddressController(req, res))
+router.route("/").put((req, res) => addressController.updateAddressController(req, res))
 
 module.exports = router
