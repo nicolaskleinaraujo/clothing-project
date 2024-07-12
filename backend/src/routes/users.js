ba0f9f506f01e266c8b31340187c527454453d77
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-12 14:33:20 -0300

    adds the createLogin to the user router

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -7,5 +7,6 @@ const validateUser = require("../middlewares/validateUser")
 router.route("/").post((req, res) => userController.createUserController(req, res))
 router.route("/").put(validateUser, (req, res) => userController.updateUserController(req, res))
 router.route("/").delete(validateUser, (req, res) => userController.deleteUserController(req, res))
+router.route("/login").post((req, res) => userController.createLoginController(req, res))
 
 module.exports = router
