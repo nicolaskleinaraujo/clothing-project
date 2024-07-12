7280724b37cbb005f7b22c7eaa6640c8916b9431
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-12 12:45:36 -0300

    added the middleware on the router

--- backend/src/routes/address.js
+++ backend/src/routes/address.js
@@ -2,10 +2,11 @@
 const router = require("express").Router()
 const addressController = require("../controllers/address/index")
 const validateUser = require("../middlewares/validateUser")
+const validateToken = require("../middlewares/validateToken")
 
 // Routes
 router.route("/").post(validateUser, (req, res) => addressController.createAddressController(req, res))
-router.route("/").put(validateUser, (req, res) => addressController.updateAddressController(req, res))
+router.route("/").put(validateToken, validateUser, (req, res) => addressController.updateAddressController(req, res))
 router.route("/").delete(validateUser, (req, res) => addressController.deleteAddressController(req, res))
 
 module.exports = router
