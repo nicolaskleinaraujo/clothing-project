1cf5c75290e26d08a7c3d084701d0297247e3b86
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-12 14:48:34 -0300

    adds validateToken on deleteUser route

--- backend/src/routes/address.js
+++ backend/src/routes/address.js
@@ -7,6 +7,6 @@ const validateToken = require("../middlewares/validateToken")
 // Routes
 router.route("/").post(validateUser, (req, res) => addressController.createAddressController(req, res))
 router.route("/").put(validateToken, validateUser, (req, res) => addressController.updateAddressController(req, res))
-router.route("/").delete(validateUser, (req, res) => addressController.deleteAddressController(req, res))
+router.route("/").delete(validateToken, validateUser, (req, res) => addressController.deleteAddressController(req, res))
 
 module.exports = router
