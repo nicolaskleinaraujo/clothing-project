1d937750f969c04432d07090d2776c88b9965b7f
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-12 14:56:53 -0300

    adds validateToken on updateUser route

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -2,10 +2,11 @@
 const router = require("express").Router()
 const userController = require("../controllers/user/index")
 const validateUser = require("../middlewares/validateUser")
+const validateToken = require("../middlewares/validateToken")
 
 // Routes
 router.route("/").post((req, res) => userController.createUserController(req, res))
-router.route("/").put(validateUser, (req, res) => userController.updateUserController(req, res))
+router.route("/").put(validateToken, validateUser, (req, res) => userController.updateUserController(req, res))
 router.route("/").delete(validateUser, (req, res) => userController.deleteUserController(req, res))
 router.route("/login").post((req, res) => userController.createLoginController(req, res))
 
