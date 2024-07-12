d88dd06dc61306a1709ccc634c620ed2ae3088b1
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-12 14:28:07 -0300

    added all validations

--- backend/src/controllers/user/createLogin/createLoginController.js
+++ backend/src/controllers/user/createLogin/createLoginController.js
@@ -1,5 +1,27 @@
-const createLoginController = async (req, res) => {
+const prisma = require("../../../db/client")
+const bcrypt = require("bcryptjs")
 
+const createLoginController = async (req, res) => {
+    const { email, password } = req.body
+
+    if (email === "" || password === "") {
+        res.status(400).json({ msg: "Informações insuficientes" })
+        return
+    }
+
+    const user = await prisma.user.findUnique({ where: { email } })
+
+    if (!user) {
+        res.status(404).json({ msg: "Email ou senha incorretos" })
+        return
+    }
+
+    const testPassword = bcrypt.compareSync(password, user.password)
+
+    if (!testPassword) {
+        res.status(400).json({ msg: "Email ou senha incorretos" })
+        return
+    }
 }
 
 module.exports = createLoginController
