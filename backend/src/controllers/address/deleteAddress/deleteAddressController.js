e088ed18935bb095e43acb417548633163be9e72
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-12 14:48:08 -0300

    changes deleteAddress to suits validateToken

--- backend/src/controllers/address/deleteAddress/deleteAddressController.js
+++ backend/src/controllers/address/deleteAddress/deleteAddressController.js
@@ -1,5 +1,4 @@
 const prisma = require("../../../db/client")
-const jwt = require("jsonwebtoken")
 
 const deleteAddressController = async (req, res) => {
     const {
@@ -18,26 +17,6 @@ const deleteAddressController = async (req, res) => {
     const addressExists = await prisma.address.findUnique({ where: { id } })
     if (!addressExists) {
         res.status(404).json({ msg: "Endereço não encontrado" })
-        return
-    }
-
-    // Checking the refresh token
-    const cookie = req.signedCookies.refresh
-    if (!cookie) {
-        res.status(401).json({ msg: "Sessão expirada, faça o login novamente" })
-        return
-    }
-
-    // Verifying the JWT
-    const jwtPayload = jwt.verify(cookie, process.env.JWT_SECRET)
-    if (!jwtPayload) {
-        res.status(401).json({ msg: "Sessão expirada, faça o login novamente" })
-        return
-    }
-
-    // Checking if the cookie matches the request id
-    if (jwtPayload.id != userId) {
-        res.status(401).json({ msg: "Sessão expirada, faça o login novamente" })
         return
     }
 
