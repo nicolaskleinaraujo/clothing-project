0e24a21d19576b2ddc7b5e5b9fbf279046b2867f
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-13 07:16:01 -0300

    creates index file for productControllers

---
+++ backend/src/controllers/products/index.js
@@ -0,0 +1,9 @@
+// Controllers
+const createProductController = require("./createProduct/createProductController")
+
+// Exporting all controllers
+const productController = {
+    createProductController,
+}
+
+module.exports = productController
