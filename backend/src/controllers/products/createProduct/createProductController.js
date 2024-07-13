361000a71a4a982773f7a2f13c906fd107a78b44
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-13 08:01:42 -0300

    adds create product function on createProduct

--- backend/src/controllers/products/createProduct/createProductController.js
+++ backend/src/controllers/products/createProduct/createProductController.js
@@ -24,6 +24,23 @@ const createProductController = async (req, res) => {
         res.status(400).json({ msg: "Informações insuficientes" })
         return
     }
+
+    try {
+        const product = await prisma.products.create({
+            data: {
+                name,
+                image: imageName,
+                price,
+                sizes,
+                colors,
+                quantity,
+            }
+        })
+
+        res.status(201).json({ msg: "Produto criado com sucesso", product })
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente", error })
+    }
 }
 
 module.exports = createProductController
