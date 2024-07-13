95f9fcc1a4108559a68b2ccc5956e3299c4e9a88
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-13 09:08:35 -0300

    adds static route for image serving

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
+const path = require("node:path")
 
 const users = require("./routes/users")
 const address = require("./routes/address")
@@ -22,6 +23,7 @@ app.use(
 app.get("/", (req, res) => {
     res.send("Bem-vindo a minha API, para mais informações, confira o repositório do projeto")
 })
+app.use("/images", express.static(path.resolve("product_images")))
 app.use("/users", users)
 app.use("/address", address)
 app.use("/products", products)
