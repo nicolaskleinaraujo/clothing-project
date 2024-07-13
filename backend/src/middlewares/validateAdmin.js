62e9e910e2d9ca0344586757e4e995a3ca8d9c42
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-13 10:53:32 -0300

    creates the validateAdmin middleware

--- backend/src/middlewares/validateAdmin.js
+++ backend/src/middlewares/validateAdmin.js
@@ -1,5 +1,27 @@
-const validateAdmin = (req, res, next) => {
-    
+const prisma = require("../db/client")
+
+const validateAdmin = async (req, res, next) => {
+    const userId = req.body.userId
+
+    if (isNaN(userId)) {
+        res.status(400).json({ msg: "Informações insuficientes1" })
+        return
+    }
+
+    // Checking if user exists
+    const user = await prisma.user.findUnique({ where: { id: userId } })
+
+    if (!user) {
+        res.status(404).json({ msg: "Usuario não encontrado" })
+        return
+    }
+
+    if (!user.isAdmin) {
+        res.status(401).json({ msg: "Usuario não é admin" })
+        return
+    }
+
+    next()
 }
 
 module.exports = validateAdmin
--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -1,6 +1,7 @@
 // Imports
 const router = require("express").Router()
 const productController = require("../controllers/products/index")
+const validateAdmin = require("../middlewares/validateAdmin")
 
 // Multer
 const multer = require("multer")
@@ -9,8 +10,8 @@ const upload = multer({ storage })
 
 // Routes
 router.route("/").post(upload.single("file"), (req, res) => productController.createProductController(req, res))
-router.route("/").put((req, res) => productController.updateProductController(req, res))
+router.route("/").put(validateAdmin, (req, res) => productController.updateProductController(req, res))
 router.route("/").patch(upload.single("file"), (req, res) => productController.updateImageController(req, res))
-router.route("/").delete((req, res) => productController.deleteProductController(req, res))
+router.route("/").delete(validateAdmin, (req, res) => productController.deleteProductController(req, res))
 
 module.exports = router
