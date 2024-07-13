531027530492a9a9cb1f9dacd4bad3c6ee21be0b
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-13 09:51:44 -0300

    adds all the validations needed

--- backend/src/controllers/products/updateImage/updateImageController.js
+++ backend/src/controllers/products/updateImage/updateImageController.js
@@ -1,5 +1,20 @@
-const updateImageController = async (req, res) => {
+const prisma = require("../../../db/client")
+const fs = require("node:fs")
+const path = require("node:path")
 
+const updateImageController = async (req, res) => {
+    const {
+        id,
+    } = req.body
+
+    const fileName = req.file.filename
+
+    // Checking if the provided product ID is valid
+    const product = await prisma.products.findUnique({ where: { id } })
+    if (!product) {
+        res.status(404).json({ msg: "Produto não encontrado" })
+        return
+    }
 }
 
 module.exports = updateImageController
