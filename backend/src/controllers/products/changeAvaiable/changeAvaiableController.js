a307cd116895271783f41eb6e4756d705d1e3e55
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-13 11:39:46 -0300

    adds all validations to changeAvaiable

--- backend/src/controllers/products/changeAvaiable/changeAvaiableController.js
+++ backend/src/controllers/products/changeAvaiable/changeAvaiableController.js
@@ -1,7 +1,19 @@
 const prisma = require("../../../db/client")
 
 const changeAvaiableController = async (req, res) => {
-    
+    const id = req.body.id
+
+    if (isNaN(id)) {
+        res.status(400).json({ msg: "Informações insuficientes" })
+        return
+    }
+
+    // Checks if provided product ID is valid
+    const product = await prisma.products.findUnique({ where: { id } })
+    if (!product) {
+        res.status(404).json({ msg: "Produto não encontrado" })
+        return
+    }
 }
 
 module.exports = changeAvaiableController
