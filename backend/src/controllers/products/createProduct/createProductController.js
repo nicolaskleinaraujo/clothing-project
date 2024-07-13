8167fece004fa11ddb415466ffba3ad56a563632
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-13 08:01:13 -0300

    adds all the validations on createProduct

--- backend/src/controllers/products/createProduct/createProductController.js
+++ backend/src/controllers/products/createProduct/createProductController.js
@@ -1,5 +1,29 @@
-const createProductController = async (req, res) => {
+const prisma = require("../../../db/client")
 
+const createProductController = async (req, res) => {
+    const {
+        name,
+        price,
+        sizes,
+        colors,
+        quantity,
+    } = req.body
+
+    const imageName = req.file.filename
+
+    parseInt(quantity)
+    parseFloat(price)
+
+    if (
+        name === "" ||
+        isNaN(price) ||
+        sizes === "" ||
+        colors === "" ||
+        isNaN(quantity)
+    ) {
+        res.status(400).json({ msg: "Informações insuficientes" })
+        return
+    }
 }
 
 module.exports = createProductController
