77bc09ba6e3ed300b3951612e3eeb990d2dcc848
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-14 10:45:01 -0300

    adds the router export on categories router

--- backend/src/routes/categories.js
+++ backend/src/routes/categories.js
@@ -4,3 +4,5 @@ const categoryController = require("../controllers/category/index")
 
 // Routes
 router.route("/").post((req, res) => categoryController.createCategoryController(req, res))
+
+module.exports = router
