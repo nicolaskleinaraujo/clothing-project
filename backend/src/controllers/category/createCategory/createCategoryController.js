1721f7b69cddbad5ab52791b165413a8cc1ad0b2
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-14 10:34:01 -0300

    finishes the createCategoryController

--- backend/src/controllers/category/createCategory/createCategoryController.js
+++ backend/src/controllers/category/createCategory/createCategoryController.js
@@ -13,6 +13,14 @@ const createCategoryController = async (req, res) => {
         res.status(400).json({ msg: "Nome de categoria já esta em uso" })
         return
     }
+
+    try {
+        const category = await prisma.categories.create({ data: { name } })
+
+        res.status(201).json({ msg: "Categoria criada com sucesso", category })
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente" })
+    }
 }
 
 module.exports = createCategoryController
