54f05afc4a54549d80430e4df0da0f5fb68d9744
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-15 12:38:59 -0300

    adds all validations needed

--- backend/src/controllers/category/deleteCategory/deleteCategoryController.js
+++ backend/src/controllers/category/deleteCategory/deleteCategoryController.js
@@ -1,7 +1,18 @@
 const prisma = require("../../../db/client")
 
 const deleteCategoryController = async (req, res) => {
-    
+    const { id } = req.body
+
+    if (isNaN(id)) {
+        res.status(400).json({ msg: "Informações insuficientes" })
+        return
+    }
+
+    const category = await prisma.categories.findUnique({ where: { id } })
+    if (!category) {
+        res.status(404).json({ msg: "Categoria não encontrada" })
+        return
+    }
 }
 
 module.exports = deleteCategoryController
