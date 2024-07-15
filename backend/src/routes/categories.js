fd0b9c5df72a59b1a42d4e742c437799aa703215
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-15 10:26:39 -0300

    adds the deleteCategory to the router

--- backend/src/routes/categories.js
+++ backend/src/routes/categories.js
@@ -5,5 +5,6 @@ const categoryController = require("../controllers/category/index")
 // Routes
 router.route("/").post((req, res) => categoryController.createCategoryController(req, res))
 router.route("/").put((req, res) => categoryController.updateCategoryController(req, res))
+router.route("/").delete((req, res) => categoryController.deleteCategoryController(req, res))
 
 module.exports = router
