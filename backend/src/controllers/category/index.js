a15058790a366b1d394ec3795ff5ca9baad306d5
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-15 10:25:57 -0300

    adds deleteCategory to the index file

--- backend/src/controllers/category/index.js
+++ backend/src/controllers/category/index.js
@@ -1,9 +1,11 @@
 const createCategoryController = require("./createCategory/createCategoryController")
+const deleteCategoryController = require("./deleteCategory/deleteCategoryController")
 const updateCategoryController = require("./updateCategory/updateCategoryController")
 
 const categoryController = {
     createCategoryController,
     updateCategoryController,
+    deleteCategoryController,
 }
 
 module.exports = categoryController
