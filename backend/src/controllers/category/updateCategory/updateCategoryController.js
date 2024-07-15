c663dab3530c6b4a03d9f789f67fbf39e66599bd
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-15 09:52:50 -0300

    adds all the validations

--- backend/src/controllers/category/updateCategory/updateCategoryController.js
+++ backend/src/controllers/category/updateCategory/updateCategoryController.js
@@ -1,7 +1,26 @@
 const prisma = require("../../../db/client")
 
-const updateCategoryController = (req, res) => {
-    
+const updateCategoryController = async (req, res) => {
+    const { id, name } = req.body
+
+    if (isNaN(id) || name === "") {
+        res.status(400).json({ msg: "Informações insuficientes" })
+        return
+    }
+
+    // Checks if the category name is in use
+    const nameInUse = await prisma.categories.findUnique({ where: { name } })
+    if (nameInUse.id != id) {
+        res.status(400).json({ msg: "Nome já em uso" })
+        return
+    }
+
+    // Checks if the provided category ID is valid
+    const category = await prisma.categories.findUnique({ where: { id } })
+    if (!category) {
+        res.status(404).json({ msg: "Categoria não encontrada" })
+        return
+    }
 }
 
 module.exports = updateCategoryController
