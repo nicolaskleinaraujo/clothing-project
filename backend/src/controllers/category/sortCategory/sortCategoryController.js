60e7641d6d2cf1339735a879b57463064389e9cb
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-16 18:23:38 -0300

    finalizes the sortCategoryController

--- backend/src/controllers/category/sortCategory/sortCategoryController.js
+++ backend/src/controllers/category/sortCategory/sortCategoryController.js
@@ -1,7 +1,13 @@
 const prisma = require("../../../db/client")
 
 const sortCategoryController = async (req, res) => {
+    try {
+        const categories = await prisma.categories.findMany()
 
+        res.status(200).json({ msg: "Pesquisa feita com sucesso", categories })
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente", error })
+    }
 }
 
 module.exports = sortCategoryController
