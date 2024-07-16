307a5d25b53998595734605446c785741d956361
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-16 15:13:26 -0300

    initializes the sortProductController

--- backend/src/controllers/product/sortProduct/sortProductController.js
+++ backend/src/controllers/product/sortProduct/sortProductController.js
@@ -1,7 +1,30 @@
 const prisma = require("../../../db/client")
 
 const sortProductController = async (req, res) => {
+    const category = req.query.category
+    const size = req.query.size
 
+    const whereClause = {}
+
+    console.log(category)
+    console.log(size)
+
+    if (category && size) {
+        whereClause.categoryId = parseInt(category)
+        whereClause.sizes = size
+    } else if (category) {
+        whereClause.categoryId = parseInt(category)
+    } else if (size) {
+        whereClause.sizes = size
+    }
+
+    console.log(whereClause)
+
+    const products = await prisma.products.findMany({
+        where: whereClause
+    })
+
+    res.status(200).json({ msg: "Feito", products })
 }
 
 module.exports = sortProductController
