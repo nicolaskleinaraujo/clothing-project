62f3e906621281c7e8a036e49b0ce577931f5071
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-16 18:21:07 -0300

    adds the sortCategoryController to the router

--- backend/src/controllers/category/index.js
+++ backend/src/controllers/category/index.js
@@ -1,11 +1,13 @@
 const createCategoryController = require("./createCategory/createCategoryController")
 const deleteCategoryController = require("./deleteCategory/deleteCategoryController")
+const sortCategoryController = require("./sortCategory/sortCategoryController")
 const updateCategoryController = require("./updateCategory/updateCategoryController")
 
 const categoryController = {
     createCategoryController,
     updateCategoryController,
     deleteCategoryController,
+    sortCategoryController,
 }
 
 module.exports = categoryController
