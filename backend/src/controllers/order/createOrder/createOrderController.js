b35dfbaf3ced1b86343c8d70ec897cae0ff0c4b8
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-17 12:00:33 -0300

    finalizes the createOrderController

--- backend/src/controllers/order/createOrder/createOrderController.js
+++ backend/src/controllers/order/createOrder/createOrderController.js
@@ -3,31 +3,23 @@ const prisma = require("../../../db/client")
 const createOrderController = async (req, res) => {
     const { orderData, userId } = req.body
 
-    /*  [
-            { productId: 1, sizeId: 1, quantity: 2 },
-            { productId: 2, sizeId: 2, quantity: 1 },
-            { productId: 3, sizeId: 3, quantity: 3 }
-        ] */
-
     if (!orderData) {
         res.status(400).json({ msg: "Informações insuficientes" })
         return
     }
 
+    // Creates the order product payload to create
+    const orderProducts = orderData.map(item => ({
+        product: { connect: { id: item.productId } },
+        size: { connect: { id: item.sizeId } },
+        quantity: item.quantity,
+    }))
+
     try {
         const order = await prisma.orders.create({
             data: {
-                orderProducts: {
-                    create: orderData.map(item => ({
-                        product: {
-                            connect: { id: item.productId },
-                        },
-                        size: {
-                            connect: { id: item.sizeId },
-                        },
-                        quantity: item.quantity,
-                    }))
-                }
+                orderProducts: { create: orderProducts },
+                userId,
             }
         })
 
