0c523cda64d56164e0aee77ae3f8a1b634b7ab7e
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-17 11:37:42 -0300

    adds all the validations on createOrder

--- backend/src/controllers/order/createOrder/createOrderController.js
+++ backend/src/controllers/order/createOrder/createOrderController.js
@@ -1,9 +1,18 @@
 const prisma = require("../../../db/client")
 
 const createOrderController = async (req, res) => {
-    const {
+    const { orderData } = req.body
 
-    } = req.body
+    /*  [
+            { productId: 1, sizeId: 1, quantity: 2 },
+            { productId: 2, sizeId: 2, quantity: 1 },
+            { productId: 3, sizeId: 3, quantity: 3 }
+        ] */
+
+    if (!orderData) {
+        res.status(400).json({ msg: "Informações insuficientes" })
+        return
+    }
 }
 
 module.exports = createOrderController
