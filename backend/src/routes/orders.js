fc78c7eb9f22832fb5832aabbebec5ec232daca4
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-17 11:49:39 -0300

    adds the index controller to the router

--- backend/src/routes/orders.js
+++ backend/src/routes/orders.js
@@ -1,6 +1,8 @@
 // Imports
 const router = require("express").Router()
+const orderController = require("../controllers/order/index")
 
 // Routes
+router.route("/").post((req, res) => orderController.createOrderController(req, res))
 
 module.exports = router
