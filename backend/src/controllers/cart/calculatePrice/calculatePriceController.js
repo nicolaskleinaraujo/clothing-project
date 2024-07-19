a8a087efcfefa9be1ace3c867b3e5c2f28b3098d
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-19 14:41:06 -0300

    adds all the validations to the calculatePrice

--- backend/src/controllers/cart/calculatePrice/calculatePriceController.js
+++ backend/src/controllers/cart/calculatePrice/calculatePriceController.js
@@ -1,5 +1,10 @@
 const calculatePriceController = async(req, res) => {
-    
+    const cart = req.signedCookies.cart
+
+    if (!cart) {
+        res.status(400).json({ msg: "Informações insuficientes" })
+        return
+    }
 }
 
 module.exports = calculatePriceController
