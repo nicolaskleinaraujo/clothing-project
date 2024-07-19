9830e893a7e11db49f2fa6289f721d903b1da57e
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-19 14:23:46 -0300

    adds middlewares to the orders router

--- backend/src/routes/orders.js
+++ backend/src/routes/orders.js
@@ -1,9 +1,12 @@
 // Imports
 const router = require("express").Router()
 const orderController = require("../controllers/order/index")
+const validateUser = require("../middlewares/validateUser")
+const validateToken = require("../middlewares/validateToken")
+const validateAdmin = require("../middlewares/validateAdmin")
 
 // Routes
-router.route("/").post((req, res) => orderController.createOrderController(req, res))
-router.route("/").delete((req, res) => orderController.deleteOrderController(req, res))
+router.route("/").post(validateToken, validateUser, (req, res) => orderController.createOrderController(req, res))
+router.route("/").delete(validateToken, validateAdmin, (req, res) => orderController.deleteOrderController(req, res))
 
 module.exports = router
