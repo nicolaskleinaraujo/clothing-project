f8181eb372b328b61d0aa70363d3dc15a90ef393
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-19 13:41:50 -0300

    adds all the validations

--- backend/src/controllers/order/deleteOrder/deleteOrderController.js
+++ backend/src/controllers/order/deleteOrder/deleteOrderController.js
@@ -1,7 +1,12 @@
 const prisma = require("../../../db/client")
 
 const deleteOrderController = async(req, res) => {
-    
+    const { id } = req.body
+
+    if (isNaN(id)) {
+        res.status(400).json({ msg: "Informações insuficientes" })
+        return
+    }
 }
 
 module.exports = deleteOrderController
