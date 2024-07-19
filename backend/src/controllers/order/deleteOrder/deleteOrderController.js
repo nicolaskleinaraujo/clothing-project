d0ffaa911490a5caf7d322cf327e55b88d383dee
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-19 13:42:12 -0300

    finalizes the deleteOrderController

--- backend/src/controllers/order/deleteOrder/deleteOrderController.js
+++ backend/src/controllers/order/deleteOrder/deleteOrderController.js
@@ -7,6 +7,15 @@ const deleteOrderController = async(req, res) => {
         res.status(400).json({ msg: "Informações insuficientes" })
         return
     }
+
+    try {
+        await prisma.orderProduct.deleteMany({ where: { orderId: id } })
+        await prisma.orders.delete({ where: { id } })
+
+        res.status(200).json({ msg: "Pedido excluido com sucesso" })
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente", error })
+    }
 }
 
 module.exports = deleteOrderController
