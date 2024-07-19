412c5fe75fcdcd0ec89bfd94dc82ad4f04bd42a9
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-19 13:37:20 -0300

    adds the deleteOrderController to the router

--- backend/src/controllers/order/index.js
+++ backend/src/controllers/order/index.js
@@ -1,7 +1,9 @@
 const createOrderController = require("./createOrder/createOrderController")
+const deleteOrderController = require("./deleteOrder/deleteOrderController")
 
 const orderController = {
     createOrderController,
+    deleteOrderController,
 }
 
 module.exports = orderController
--- backend/src/routes/orders.js
+++ backend/src/routes/orders.js
@@ -4,5 +4,6 @@ const orderController = require("../controllers/order/index")
 
 // Routes
 router.route("/").post((req, res) => orderController.createOrderController(req, res))
+router.route("/").delete((req, res) => orderController.deleteOrderController(req, res))
 
 module.exports = router
