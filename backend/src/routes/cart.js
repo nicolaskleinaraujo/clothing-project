31d1db500b3cd3e2e79af022fca332de98d5627e
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-24 12:10:49 -0300

    removes validateAddress from cart router

--- backend/src/routes/cart.js
+++ backend/src/routes/cart.js
@@ -3,10 +3,9 @@ const router = require("express").Router()
 const cartController = require("../controllers/cart/index")
 const validateUser = require("../middlewares/validateUser")
 const validateToken = require("../middlewares/validateToken")
-const validateAddress = require("../middlewares/validateAddress")
 
 // Routes
 router.route("/").post(validateToken, validateUser, (req, res) => cartController.addProductController(req, res))
-router.route("/").get(validateToken, validateUser, validateAddress, (req, res) => cartController.calculatePriceController(req, res))
+router.route("/").get(validateToken, validateUser, (req, res) => cartController.calculatePriceController(req, res))
 
 module.exports = router
