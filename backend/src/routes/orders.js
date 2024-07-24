c89e257643acdf1c6d429b16419279cb11af768c
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-24 11:54:18 -0300

    adds the validateAddress to createOrder route

--- backend/src/routes/orders.js
+++ backend/src/routes/orders.js
@@ -4,9 +4,10 @@ const orderController = require("../controllers/order/index")
 const validateUser = require("../middlewares/validateUser")
 const validateToken = require("../middlewares/validateToken")
 const validateAdmin = require("../middlewares/validateAdmin")
+const validateAddress = require("../middlewares/validateAddress")
 
 // Routes
-router.route("/").post(validateToken, validateUser, (req, res) => orderController.createOrderController(req, res))
+router.route("/").post(validateToken, validateUser, validateAddress, (req, res) => orderController.createOrderController(req, res))
 router.route("/").delete(validateToken, validateAdmin, (req, res) => orderController.deleteOrderController(req, res))
 
 module.exports = router
