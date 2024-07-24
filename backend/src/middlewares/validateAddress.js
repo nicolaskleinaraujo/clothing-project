16b9903466b6c61c24fc0af9c597d04748c741ef
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-24 11:39:53 -0300

    finalizes the validateAddress middleware

--- backend/src/middlewares/validateAddress.js
+++ backend/src/middlewares/validateAddress.js
@@ -1,7 +1,30 @@
 const prisma = require("../db/client")
 
 const validateAddress = async (req, res, next) => {
+    const userId = req.body.userId
 
+    if (isNaN(userId)) {
+        res.status(400).json({ msg: "Informações insuficientes" })
+        return
+    }
+
+    // Checking if user exists
+    const user = await prisma.user.findUnique({
+        where: { id: userId },
+        include: { Address: true },
+    })
+
+    if (!user) {
+        res.status(404).json({ msg: "Usuario não encontrado" })
+        return
+    }
+
+    if (!user.Address[0].cep) {
+        res.status(401).json({ msg: "Usuario não possui endereço de entrega" })
+        return
+    }
+
+    next()
 }
 
 module.exports = validateAddress
