9316155f7774ce6f6e424eb0deab8310c6e9c3bf
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-24 12:23:00 -0300

    finalizes the removeProductController

--- backend/src/controllers/cart/removeProduct/removeProductController.js
+++ backend/src/controllers/cart/removeProduct/removeProductController.js
@@ -1,5 +1,25 @@
 const removeProductController = async(req, res) => {
+    const cart = req.signedCookies.cart
+    const id = parseInt(req.body.id)
 
+    if (!cart || isNaN(id)) {
+        res.status(400).json({ msg: "Infomações insuficientes" })
+        return
+    }
+
+    try {
+        const updatedCart = cart.filter(item => item.id !== id)
+
+        res.cookie("cart", updatedCart, {
+            httpOnly: true,
+            signed: true,
+            secure: true,
+            sameSite: "none",
+        })
+        res.status(200).json({ msg: "Produto removido do carrinho" })
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente" })
+    }
 }
 
 module.exports = removeProductController
