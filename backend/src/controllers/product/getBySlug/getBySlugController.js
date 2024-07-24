9e06aef5899112d1866f4e955016326de6f2fdd7
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-24 11:18:48 -0300

    adds unique value to the product slug

--- backend/src/controllers/product/getBySlug/getBySlugController.js
+++ backend/src/controllers/product/getBySlug/getBySlugController.js
@@ -1,7 +1,15 @@
 const prisma = require("../../../db/client")
 
 const getBySlugController = async() => {
+    const slug = req.params.slug
 
+    console.log(slug)
+
+    try {
+        const product = await prisma.products.findUnique({ where: { slug } })
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente", error })
+    }
 }
 
 module.exports = getBySlugController
