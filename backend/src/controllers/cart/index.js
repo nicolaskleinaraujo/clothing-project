9d4fb79cc1638ea09d8b6f5b650578be55a0fd85
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-24 12:33:35 -0300

    adds removeProductController to cart router

--- backend/src/controllers/cart/index.js
+++ backend/src/controllers/cart/index.js
@@ -1,11 +1,13 @@
 // Controllers
 const addProductController = require("./addProduct/addProductController")
 const calculatePriceController = require("./calculatePrice/calculatePriceController")
+const removeProductController = require("./removeProduct/removeProductController")
 
 // Exporting all controllers
 const cartController = {
     addProductController,
     calculatePriceController,
+    removeProductController,
 }
 
 module.exports = cartController
