3bd8465198f3bf08524e8f55f7b09cce5dbf24c3
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-24 11:22:06 -0300

    finalizes the getBySlugController

--- backend/src/controllers/product/getBySlug/getBySlugController.js
+++ backend/src/controllers/product/getBySlug/getBySlugController.js
@@ -1,12 +1,12 @@
 const prisma = require("../../../db/client")
 
-const getBySlugController = async() => {
+const getBySlugController = async(req, res) => {
     const slug = req.params.slug
 
-    console.log(slug)
-
     try {
-        const product = await prisma.products.findUnique({ where: { slug } })
+        const product = await prisma.products.findFirst({ where: { slug }, include: { sizes: true } })
+
+        res.status(200).json({ msg: "Produto achado com sucesso", product })
     } catch (error) {
         res.status(500).json({ msg: "Erro interno, tente novamente", error })
     }
