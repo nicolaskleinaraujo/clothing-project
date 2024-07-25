9bdbc8a5397ea50a57cc60947726d680a7f4d965
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-25 11:27:56 -0300

    adds all the validations needed

--- backend/src/controllers/coupon/createCoupon/createCouponController.js
+++ backend/src/controllers/coupon/createCoupon/createCouponController.js
@@ -1,5 +1,12 @@
-const createCouponController = async(req, res) => {
+const prisma = require("../../../db/client")
 
+const createCouponController = async(req, res) => {
+    const code = req.body.code
+
+    if (code === "") {
+        res.status(400).json({ msg: "Informações insuficientes" })
+        return
+    }
 }
 
 module.exports = createCouponController
