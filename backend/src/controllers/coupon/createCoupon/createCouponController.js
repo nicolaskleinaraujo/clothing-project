a41b4c8028c7c1d95b9c213cf9325a13a440ed92
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-25 11:32:48 -0300

    finalizes the createCouponController

--- backend/src/controllers/coupon/createCoupon/createCouponController.js
+++ backend/src/controllers/coupon/createCoupon/createCouponController.js
@@ -9,7 +9,15 @@ const createCouponController = async(req, res) => {
     }
 
     try {
-        const codeAlreadyExists = prisma.coupon.
+        const couponAlreadyExists = await prisma.coupon.findUnique({ where: { code } })
+        if (couponAlreadyExists) {
+            res.status(400).json({ msg: "Nome de cupom já cadastrado" })
+            return
+        }
+
+        const coupon = await prisma.coupon.create({ data: { code } })
+        
+        res.status(201).json({ msg: "Cupom criado com sucesso", coupon })
     } catch (error) {
         res.status(500).json({ msg: "Erro interno, tente novamente", error })
     }
