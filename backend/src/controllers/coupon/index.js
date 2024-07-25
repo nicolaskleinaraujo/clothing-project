e497c52ed7d0718841a6e94d401abedc3c53cd3d
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-25 17:44:40 -0300

    adds the deleteCoupon to the coupon router

--- backend/src/controllers/coupon/index.js
+++ backend/src/controllers/coupon/index.js
@@ -1,9 +1,11 @@
 const createCouponController = require("./createCoupon/createCouponController")
+const deleteCouponController = require("./deleteCoupon/deleteCouponController")
 const invalidateCouponController = require("./invalidateCoupon/invalidateCouponController")
 
 const couponController = {
     createCouponController,
     invalidateCouponController,
+    deleteCouponController,
 }
 
 module.exports = couponController
--- backend/src/routes/coupons.js
+++ backend/src/routes/coupons.js
@@ -6,5 +6,6 @@ const validateAdmin = require("../middlewares/validateAdmin")
 // Routes
 router.route("/").post(validateAdmin, (req, res) => couponController.createCouponController(req, res))
 router.route("/").patch(validateAdmin, (req, res) => couponController.invalidateCouponController(req, res))
+router.route("/").delete(validateAdmin, (req, res) => couponController.deleteCouponController(req, res))
 
 module.exports = router
