fd1d43b0dd0877bfc0497b3b73dcd28073b7368c
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-25 17:46:47 -0300

    adds all validations needed

--- backend/src/controllers/coupon/deleteCoupon/deleteCouponController.js
+++ backend/src/controllers/coupon/deleteCoupon/deleteCouponController.js
@@ -1,7 +1,22 @@
 const prisma = require("../../../db/client")
 
 const deleteCouponController = async(req, res) => {
+    const id = parseInt(req.body.id)
 
+    if (isNaN(id)) {
+        res.status(400).json({ msg: "Informações insuficientes" })
+        return
+    }
+
+    try {
+        const coupon = await prisma.coupon.findUnique({ where: { id } })
+        if (!coupon) {
+            res.status(404).json({ msg: "Cupom não encontrado" })
+            return
+        }
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente", error })
+    }
 }
 
 module.exports = deleteCouponController
