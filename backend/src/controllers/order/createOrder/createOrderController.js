30a522beefb7e20573804a5919f8fad86d469e08
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-26 23:44:36 -0300

    adds payment config to createOrder

--- backend/src/controllers/order/createOrder/createOrderController.js
+++ backend/src/controllers/order/createOrder/createOrderController.js
@@ -1,5 +1,6 @@
 const prisma = require("../../../db/client")
 const calculateShipping = require("../../../config/shipping")
+const createPayment = require("../../../config/mercadopago")
 
 const createOrderController = async (req, res) => {
     const cart = req.signedCookies.cart
@@ -10,19 +11,33 @@ const createOrderController = async (req, res) => {
         return
     }
 
-    // Creates the order product payload to create
-    const orderProducts = cart.map(item => ({
-        product: { connect: { id: item.productId } },
-        size: { connect: { id: item.sizeId } },
-        quantity: item.quantity,
-    }))
+    // Creates the payment payload
+    const paymentPayload = {
+        products: [],
+        userName: "",
+        userEmail: "",
+    }
+
+    // Gets the user address to calculate the shipping price
+    const user = await prisma.user.findUnique({ where: { id: userId }, include: { Address: true } })
+    const shipping = await calculateShipping(user.Address[0].cep)
+    let price = parseFloat(shipping.price)
+
+    paymentPayload.userName = user.firstName
+    paymentPayload.userEmail = user.email
+
+    paymentPayload.products.push({
+        name: "Frete",
+        price: parseFloat(shipping.price),
+    })
 
     // Validates the integrity of the product informed by the client
-    let price = 0
-    for (let index = 0; index < orderProducts.length; index++) {
-        const id = cart[index].productId
+    for (let index = 0; index < cart.length; index++) {
+        const product = await prisma.products.findUnique({
+            where: { id: cart[index].productId },
+            include: { sizes: true }
+        })
 
-        const product = await prisma.products.findUnique({ where: { id }, include: { sizes: true } })
         if (!product) {
             res.status(400).json({ msg: "Pedido invalido" })
             return
@@ -45,22 +60,30 @@ const createOrderController = async (req, res) => {
         }
 
         // Updates the product stock
-        const quantityToUpdate = product.quantity - cart[index].quantity
+        // const quantityToUpdate = product.quantity - cart[index].quantity
         await prisma.products.update({
-            where: { id },
-            data: { quantity: quantityToUpdate },
+            where: { id: cart[index].productId },
+            data: { quantity: product.quantity - cart[index].quantity },
         })
 
         // Calculates the order price
-        price += product.price
+        paymentPayload.products.push({
+            name: `${cart[index].quantity}x ${product.name}`,
+            price: product.price * cart[index].quantity,
+        })
+        price += product.price * cart[index].quantity
     }
 
-    // Gets the user address to calculate the shipping
-    const user = await prisma.user.findUnique({ where: { id: userId }, include: { Address: true } })
-    const shipping = await calculateShipping(user.Address[0].cep)
-    price += parseInt(shipping.price)
-
     try {
+        const payment = await createPayment(paymentPayload)
+
+        // Creates the order products payload to create
+        const orderProducts = cart.map(item => ({
+            product: { connect: { id: item.productId } },
+            size: { connect: { id: item.sizeId } },
+            quantity: item.quantity,
+        }))
+
         const order = await prisma.orders.create({
             data: {
                 orderProducts: { create: orderProducts },
@@ -69,7 +92,7 @@ const createOrderController = async (req, res) => {
             }
         })
 
-        res.status(201).json({ msg: "Pedido feito com sucesso", order })
+        res.status(201).json({ msg: "Pedido feito com sucesso", order, payment })
     } catch (error) {
         res.status(500).json({ msg: "Erro interno, tente novamente", error })
     }
