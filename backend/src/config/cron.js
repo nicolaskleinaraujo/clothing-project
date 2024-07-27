a5d0d6f97770cec87f432653e6789ce41a17120f
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-27 18:32:59 -0300

    finalizes deleteExpiredOrders file

--- backend/src/config/cron.js
+++ backend/src/config/cron.js
@@ -1,7 +1,23 @@
 const prisma = require("../db/client")
+const dayjs = require("dayjs")
 
 const deleteExpiredOrders = async() => {
-    
+    try {
+        // Takes the current time and removes 5 minutes
+        const cutOffTime = dayjs().subtract(5, "minutes").toDate()
+
+        // Deletes all unpaid orders created more than 5 minutes ago
+        await prisma.orders.deleteMany({
+            where: {
+                paid: false,
+                created_at: {
+                    lt: cutOffTime
+                },
+            },
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }   
 
 module.exports = deleteExpiredOrders
