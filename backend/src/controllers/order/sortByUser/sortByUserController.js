e0714ebf801b8b370e9cf7fddb85a113a16ba591
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-27 15:34:41 -0300

    adds all validations needed

--- backend/src/controllers/order/sortByUser/sortByUserController.js
+++ backend/src/controllers/order/sortByUser/sortByUserController.js
@@ -1,7 +1,22 @@
 const prisma = require("../../../db/client")
 
 const sortByUserController = async(req, res) => {
-    
+    const { id, userId } = req.body
+
+    if (isNaN(id) || isNaN(userId)) {
+        res.status(400).json({ msg: "Informações insuficientes" })
+        return
+    }
+
+    try {
+        const user = await prisma.user.findUnique({ where: { id: userId } })
+        if (id != userId && !user.isAdmin) {
+            res.status(401).json({ msg: "Usuario não é admin" })
+            return
+        }
+    } catch (error) {
+        res.status(500).json({ msg: "Erro intero, tente novamente" })
+    }
 }
 
 module.exports = sortByUserController
