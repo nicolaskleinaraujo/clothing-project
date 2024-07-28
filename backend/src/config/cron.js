126f77a876ef474e0f10ca7c7b5bdc3ac98bb9a8
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-27 21:13:06 -0300

    adds orderProduct deletion on deleteExpiredOrders

--- backend/src/config/cron.js
+++ backend/src/config/cron.js
@@ -6,6 +6,18 @@ const deleteExpiredOrders = async() => {
         // Takes the current time and removes 5 minutes
         const cutOffTime = dayjs().subtract(5, "minutes").toDate()
 
+        // Deletes all products from unpaid orders created more than 5 minutes ago
+        await prisma.orderProduct.deleteMany({
+            where: {
+                order: {
+                    paid: false,
+                    created_at: {
+                        lt: cutOffTime
+                    },
+                },
+            },
+        })
+
         // Deletes all unpaid orders created more than 5 minutes ago
         await prisma.orders.deleteMany({
             where: {
@@ -18,6 +30,6 @@ const deleteExpiredOrders = async() => {
     } catch (error) {
         console.log(error)
     }
-}   
+}
 
 module.exports = deleteExpiredOrders
