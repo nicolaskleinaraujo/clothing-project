a575b3f572369c8b12b322cc85c4d65fbf7a1859
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-30 14:47:41 -0300

    creates the basic structure on login

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,9 +1,31 @@
+// CSS
 import styles from "./Login.module.css"
 
 const Login = () => {
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        console.log("Handled!")
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className={styles.login}>
+            <img src="https://via.placeholder.com/500x650" alt="Imagem Roupa" />
+
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <p>Email</p>
+                    <input type="email" />
+                </label>
+
+                <label>
+                    <p>Senha</p>
+                    <input type="password" />
+                </label>
+
+                <input type="submit" value="Login" />
+
+                <p>Não tem uma conta? <a href="/register">Criar</a></p>
+            </form>
         </div>
     )
 }
