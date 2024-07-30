c569e411408b1d64ee0ab1545eef5c2e7662c1b8
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-30 13:28:35 -0300

    adds basic css to the navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,23 +3,29 @@ import styles from "./Navbar.module.css"
 
 const Navbar = () => {
     return (
-        <div>
+        <div className={styles.navbar}>
             <img src="https://via.assets.so/img.jpg?w=100&h=100&tc=black&bg=#cecece&t=logo" alt="Logo do Projeto" />
-            <div>
-                <button>Categorias</button>
-                <div>
-                    <a href="#">Categoria 1</a>
+
+            <div className={styles.category}>
+                <button>Categorias&#8595;</button>
+                <div className={styles.category_content}>
+                    <a href="#">Masculini</a>
+                    <a href="#">Feminino</a>
+                    <a href="#">Camisas</a>
+                    <a href="#">Calças</a>
+                    <a href="#">meias</a>
+                    <a href="#">Cuecas</a>
                 </div>
             </div>
 
-            <div>
-                <button>Tamanhos</button>
-                <div>
+            <div className={styles.size}>
+                <button>Tamanhos&#8595;</button>
+                <div className={styles.size_content}>
                     <a href="#">Tamanho 1</a>
                 </div>
             </div>
 
-            <div>
+            <div className={styles.account}>
                 <a href="#">conta</a>
                 <a href="#">pedidos</a>
             </div>
