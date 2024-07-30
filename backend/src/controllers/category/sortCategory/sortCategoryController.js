defcda3f399283d8d7eb38e271eabf496d89031c
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-30 17:12:14 -0300

    adds sizes response on sortCategory

--- backend/src/controllers/category/sortCategory/sortCategoryController.js
+++ backend/src/controllers/category/sortCategory/sortCategoryController.js
@@ -3,8 +3,9 @@ const prisma = require("../../../db/client")
 const sortCategoryController = async (req, res) => {
     try {
         const categories = await prisma.categories.findMany()
+        const sizes = await prisma.sizes.findMany()
 
-        res.status(200).json({ msg: "Pesquisa feita com sucesso", categories })
+        res.status(200).json({ msg: "Pesquisa feita com sucesso", categories, sizes })
     } catch (error) {
         res.status(500).json({ msg: "Erro interno, tente novamente", error })
     }
--- backend/src/controllers/category/sortSize/sortSizeController.js
+++
@@ -1,7 +0,0 @@
-const prisma = require("../../../db/client")
-
-const sortSizeController = async() => {
-    
-}
-
-module.exports = sortSizeController
