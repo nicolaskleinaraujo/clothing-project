5b7e232839c0e9adf0ba4da39ecf7aa20fa62428
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-30 12:00:53 -0300

    created the basic structure on navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,25 @@ import styles from "./Navbar.module.css"
 const Navbar = () => {
     return (
         <div>
-            <h1>Navbar</h1>
+            <img src="https://via.assets.so/img.jpg?w=100&h=100&tc=black&bg=#cecece&t=logo" alt="Logo do Projeto" />
+            <div>
+                <button>Categorias</button>
+                <div>
+                    <a href="#">Categoria 1</a>
+                </div>
+            </div>
+
+            <div>
+                <button>Tamanhos</button>
+                <div>
+                    <a href="#">Tamanho 1</a>
+                </div>
+            </div>
+
+            <div>
+                <a href="#">conta</a>
+                <a href="#">pedidos</a>
+            </div>
         </div>
     )
 }
