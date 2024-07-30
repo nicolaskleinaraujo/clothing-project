a0c1098c8c1c3ef01354b524c8f67c84928b3cdb
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-30 16:10:16 -0300

    adds react-toastify to the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,20 @@ import './App.css'
 // Router
 import Router from './utils/Router'
 
+// Modules
+import { Flip, ToastContainer } from 'react-toastify'
+
 function App() {
   return (
     <>
+      <ToastContainer 
+        position="bottom-center" 
+        autoClose={1500} 
+        pauseOnHover={false} 
+        closeOnClick 
+        transition={Flip} 
+      />
+
       <Router />
     </>
   )
