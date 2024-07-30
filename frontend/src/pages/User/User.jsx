010092467c8c469bb3dac2035aeb1d2b920d04cd
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-30 14:32:17 -0300

    creates the user page

---
+++ frontend/src/pages/User/User.jsx
@@ -0,0 +1,11 @@
+import styles from "./User.module.css"
+
+const User = () => {
+    return (
+        <div>
+            <h1>User</h1>
+        </div>
+    )
+}
+
+export default User
--- frontend/src/utils/Router.jsx
+++ frontend/src/utils/Router.jsx
@@ -4,11 +4,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 // Components
 import Navbar from "../components/Navbar/Navbar"
 
+// Pages
+import User from "../pages/User/User"
+
 const Router = () => {
     return (
         <>
             <BrowserRouter>
                 <Navbar />
+                <Routes>
+                    <Route path="/user" element={<User />} />
+                </Routes>
             </BrowserRouter>
         </>
     )
