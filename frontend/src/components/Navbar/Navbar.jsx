4bc32464a20fdede4456f51ed6ddb0217bef9856
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-30 11:38:04 -0300

    adds the navbar to the main router

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+// CSS
+import styles from "./Navbar.module.css"
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    )
+}
+
+export default Navbar
--- frontend/src/utils/Router.jsx
+++ frontend/src/utils/Router.jsx
@@ -1,11 +1,14 @@
 // Modules
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+// Components
+import Navbar from "../components/Navbar/Navbar"
+
 const Router = () => {
     return (
         <>
             <BrowserRouter>
-                
+                <Navbar />
             </BrowserRouter>
         </>
     )
