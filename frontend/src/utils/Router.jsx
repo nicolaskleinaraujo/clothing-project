f9912deef2d86630c2ee896a362fe98da4b1500a
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-30 11:35:44 -0300

    creates the main Router

---
+++ frontend/src/utils/Router.jsx
@@ -0,0 +1,14 @@
+// Modules
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+const Router = () => {
+    return (
+        <>
+            <BrowserRouter>
+                
+            </BrowserRouter>
+        </>
+    )
+}
+
+export default Router
