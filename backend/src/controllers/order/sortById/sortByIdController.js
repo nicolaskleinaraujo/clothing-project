c1c336e24332af8dd776dc6512c6dd907c4789d1
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-07-31 15:37:09 -0300

    finalizes the sortByIdController

--- backend/src/controllers/order/sortById/sortByIdController.js
+++ backend/src/controllers/order/sortById/sortByIdController.js
@@ -1,7 +1,23 @@
 const prisma = require("../../../db/client")
 
 const sortById = async(req, res) => {
+    const id = parseInt(req.params.id)
 
+    if (isNaN(id)) {
+        res.status(400).json({ msg: "Informações insuficientes" })
+        return
+    }
+
+    try {
+        const order = await prisma.orders.findUnique({
+            where: { id },
+            include: { user },
+        })
+
+        res.status(200).json({ msg: "Pesquisa feita com sucesso", order })
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente" })
+    }
 }
 
 module.exports = sortById
