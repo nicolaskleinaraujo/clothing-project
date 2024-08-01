dd6fa2e35774b4a4a765d5ed84633f415e6c2ad6
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-01 11:13:25 -0300

    adds more responsibility to home page

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -2,7 +2,10 @@ import axios from "axios"
 
 axios.defaults.withCredentials = true
 const dbFetch = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://9679-2804-7f4-5218-7a88-68be-1e34-8a2c-4786.ngrok-free.app",
+    headers: {
+        "ngrok-skip-browser-warning": true
+    }
 })
 
 export default dbFetch
