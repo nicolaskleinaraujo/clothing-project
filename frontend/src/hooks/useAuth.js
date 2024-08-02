12b59e3269c72202372911dd6941c5d745ed0da0
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-02 11:59:41 -0300

    hotfix the useAuth hook and UserRoute middleware

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,17 +1,23 @@
 // Modules
 import dbFetch from "../config/axios"
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
+import { UserContext } from "../context/UserContext"
 
 const useAuth = () => {
-    const [userId, setUserId] = useState(0)
-    const [isAdmin, setIsAdmin] = useState(false)
+    const { setUserId, setIsAdmin } = useContext(UserContext)
+
+    const [authUserId, setAuthUserId] = useState(0)
+    const [authIsAdmin, setAuthIsAdmin] = useState(false)
     const [loading, setLoading] = useState(true)
 
     const fetchData = async() => {
         const res = await dbFetch.post("/users/tryauth")
 
-        setIsAdmin(res.data.user.isAdmin)
+        setAuthUserId(res.data.user.id)
+        setAuthIsAdmin(res.data.user.isAdmin)
+
         setUserId(res.data.user.id)
+        setIsAdmin(res.data.user.isAdmin)
 
         setLoading(false)
     }
@@ -20,7 +26,7 @@ const useAuth = () => {
         fetchData()
     }, [])
 
-    return { userId, isAdmin, loading }
+    return { authUserId, authIsAdmin, loading }
 }
 
 export default useAuth
--- frontend/src/utils/UserRoute.jsx
+++ frontend/src/utils/UserRoute.jsx
@@ -1,16 +1,26 @@
 // Modules
 import { Outlet, Navigate } from "react-router-dom"
+import { useContext } from "react"
+import { UserContext } from "../context/UserContext"
 import useAuth from "../hooks/useAuth"
 
 const UserRoute = () => {
-    const { userId, loading } = useAuth()
-
-    if (loading) {
-        return
-    }
+    const { userId } = useContext(UserContext)
 
     if (userId === 0) {
-        return <Navigate to="/login" replace />
+        const { authUserId, loading } = useAuth()
+
+        if (loading) {
+            return
+        }
+    
+        if (userId === 0) {
+            return <Navigate to="/login" replace />
+        }
+
+        if (authUserId !== 0) {
+            return
+        }
     }
 
     return <Outlet />
