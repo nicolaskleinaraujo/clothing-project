a376fd40aed325674962bbfedee3c52e059a7c99
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-02 11:41:21 -0300

    hotfixes the useAuth custom hook

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -2,20 +2,25 @@
 import dbFetch from "../config/axios"
 import { useEffect, useState } from 'react'
 
-export const useAuth = () => {
+const useAuth = () => {
     const [userId, setUserId] = useState(0)
     const [isAdmin, setIsAdmin] = useState(false)
+    const [loading, setLoading] = useState(true)
 
     const fetchData = async() => {
         const res = await dbFetch.post("/users/tryauth")
 
         setIsAdmin(res.data.user.isAdmin)
         setUserId(res.data.user.id)
+
+        setLoading(false)
     }
 
     useEffect(() => {
         fetchData()
-    })
+    }, [])
 
-    return { userId, isAdmin }
+    return { userId, isAdmin, loading }
 }
+
+export default useAuth
