ddb686652c94df82ac32b4ac431b0a4889097f09
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-06 21:07:07 -0300

    finalizes the getByUserController

--- backend/src/controllers/address/getByUser/getByUserController.js
+++ backend/src/controllers/address/getByUser/getByUserController.js
@@ -1,7 +1,25 @@
 const prisma = require("../../../db/client")
 
 const getByUserController = async(req, res) => {
-    
+    const id = parseInt(req.body.id)
+
+    if (isNaN(id)) {
+        res.status(400).json({ msg: "Informações insuficientes" })
+        return
+    }
+
+    try {
+        const address = await prisma.address.findFirst({ where: { userId: id } })
+
+        if (!address) {
+            res.status(404).json({ msg: "Endereço não encontrado" })
+            return
+        }
+
+        res.status(200).json({ msg: "Endereço encontrado", address })
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente" })
+    }
 }
 
 module.exports = getByUserController
