44e543388445bc8c4eeca15dafb204621ff62832
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-07 19:59:40 -0300

    creates the basic create order structure

--- frontend/src/pages/CreateOrder/CreateOrder.jsx
+++ frontend/src/pages/CreateOrder/CreateOrder.jsx
@@ -6,7 +6,30 @@ import dbFetch from "../../config/axios"
 
 const CreateOrder = () => {
     return (
-        <div>CreateOrder</div>
+        <div>
+            <h2>Endereço</h2>
+            <p>...</p>
+
+            <h2>Cupom</h2>
+            <div>
+                <input
+                    type="text"
+                    name="coupon"
+                    id="coupon"
+                    placeholder="Digite o cupom"
+                />
+                <button>Adicionar Cupom</button>
+            </div>
+
+            <h2>Entrega</h2>
+            <select name="delivery" id="delivery">
+                <option value="">PAC</option>
+                <option value="">Sedex</option>
+            </select>
+
+            <h2>Detalhes</h2>
+            <p>...</p>
+        </div>
     )
 }
 
