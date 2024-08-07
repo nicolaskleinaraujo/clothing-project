7400507c7374e94d173a65a6443515f80c6060a5
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-06 21:02:34 -0300

    creates the getUserInfos function

--- frontend/src/pages/Address/Address.jsx
+++ frontend/src/pages/Address/Address.jsx
@@ -3,10 +3,27 @@ import styles from "./Address.module.css"
 
 // Modules
 import dbFetch from "../../config/axios"
+import { useState, useEffect, useContext } from "react"
+import { UserContext } from "../../context/UserContext"
 
 const Address = () => {
+    const { userId } = useContext(UserContext)
+
+    const [userAddress, setUserAddress] = useState([])
+
+    const getUserinfos = async() => {
+        const res = await dbFetch.get(`/address/${userId}`)
+        setUserAddress(res.data.address)
+    }
+
+    useEffect(() => {
+        getUserinfos()
+    }, [])
+
     return (
-        <div>Address</div>
+        <div>
+            <h1>Endereço</h1>
+        </div>
     )
 }
 
