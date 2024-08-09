6198de6772885b8dbe851b261bb33b983141bc30
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-09 11:57:48 -0300

    adds loading to the order page

--- frontend/src/pages/Order/Order.jsx
+++ frontend/src/pages/Order/Order.jsx
@@ -9,6 +9,8 @@ import advancedFormat from 'dayjs/plugin/advancedFormat'
 import { useState, useEffect, useContext } from "react"
 import { useParams, useNavigate } from "react-router-dom"
 import { UserContext } from "../../context/UserContext"
+import { LoadingContext } from "../../context/LoadingContext"
+import Loading from "../../components/Loading/Loading"
 
 // DayJS Configs
 dayjs.extend(utc);
@@ -18,6 +20,7 @@ const Order = () => {
     const { id } = useParams()
     const navigate = useNavigate()
     const { userId } = useContext(UserContext)
+    const { loading, setLoading } = useContext(LoadingContext)
 
     const [hours, setHours] = useState(0)
     const [minutes, setMinutes] = useState(0)
@@ -49,48 +52,57 @@ const Order = () => {
             const expiresIn = dayjs.utc(res.data.order.created_at).add(10, "minute")
             setHours(expiresIn.hour())
             setMinutes(expiresIn.minute())
+
+            setLoading(false)
         } catch (error) {
             console.log(error)
         }
     }
 
     useEffect(() => {
+        setLoading(true)
         getOrder()
     }, [])
 
     return (
         <div className={styles.order}>
-            { !paid && <p>Você tem até as {hours}:{minutes} de hoje para efetuar o pagamento</p> }
-            { !paid && <iframe src={paymentUrl}></iframe> }
+            { loading ? (
+                <Loading />
+            ) : (
+                <>
+                    { !paid && <p>Você tem até as {hours}:{minutes} de hoje para efetuar o pagamento</p> }
+                    { !paid && <iframe src={paymentUrl}></iframe> }
 
-            { orderPrice != 0 &&
-                <div className={styles.order_infos}>
-                    <p>Status: {
-                        paid ? received ? "Entregue" : "Pago" : "Aguardando Pagamento"
-                    }</p>
-                    <p>Preço total: R${orderPrice}</p>
-                    <p>Prazo de entrega: {shippingTime} dias</p>
-                    {trackingCode && <p>Codigo rastreamento: {trackingCode}</p>}
-                </div>
-            }
-
-            <h1>Resumo do Pedido</h1>
-            <div className={styles.order_products}>
-                { products &&
-                    products.map(product => (
-                        <div key={product.id}>
-                            <img src={`${import.meta.env.VITE_API_URL}/images/${product.product.image}`} alt="Foto do Produto" />
-                            <div>
-                                <p>{product.quantity}x {product.product.name}</p>
-                                <p>
-                                    {product.product.sizes.find(size => size.id === product.sizeId).size} | {product.color}
-                                </p>
-                            </div>
-                            <p>R${product.product.price}</p>
+                    { orderPrice != 0 &&
+                        <div className={styles.order_infos}>
+                            <p>Status: {
+                                paid ? received ? "Entregue" : "Pago" : "Aguardando Pagamento"
+                            }</p>
+                            <p>Preço total: R${orderPrice}</p>
+                            <p>Prazo de entrega: {shippingTime} dias</p>
+                            {trackingCode && <p>Codigo rastreamento: {trackingCode}</p>}
                         </div>
-                    ))
-                }
-            </div>
+                    }
+
+                    <h1>Resumo do Pedido</h1>
+                    <div className={styles.order_products}>
+                        { products &&
+                            products.map(product => (
+                                <div key={product.id}>
+                                    <img src={`${import.meta.env.VITE_API_URL}/images/${product.product.image}`} alt="Foto do Produto" />
+                                    <div>
+                                        <p>{product.quantity}x {product.product.name}</p>
+                                        <p>
+                                            {product.product.sizes.find(size => size.id === product.sizeId).size} | {product.color}
+                                        </p>
+                                    </div>
+                                    <p>R${product.product.price}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </>
+            )}
         </div>
     )
 }
