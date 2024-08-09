44d61096a2a35ff481e7b601686ee8adcb5d595f
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-09 11:53:02 -0300

    adds loading to the home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -5,38 +5,49 @@ import styles from "./Home.module.css"
 import dbFetch from "../../config/axios"
 
 // Modules
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
 import { MdReadMore } from "react-icons/md"
 import { Link } from "react-router-dom"
+import Loading from "../../components/Loading/Loading"
+import { LoadingContext } from "../../context/LoadingContext"
 
 const Home = () => {
+    const { loading, setLoading } = useContext(LoadingContext)
     const [products, setProducts] = useState([])
 
     const getProducts = async() => {
         const res = await dbFetch.get("/products")
         setProducts(res.data.products)
+        setLoading(false)
     }
 
     useEffect(() => {
+        setLoading(true)
         getProducts()
     }, [])
 
     return (
         <div className={styles.home}>
-            <div className={styles.home_products}>
-                {
-                    products && products.map(product => (
-                        <Link to={`/product/${product.slug}`} key={product.id}>
-                            <div>
-                                <img src={`${import.meta.env.VITE_API_URL}/images/${product.image}`} alt="Foto Produto" />
-                                <p>{product.name}</p>
-                                <p>R${product.price}</p>
-                                <Link to={`/product/${product.slug}`}><MdReadMore /></Link>
-                            </div>
-                        </Link>
-                    ))
-                }
-            </div>
+            { loading ? (
+                <Loading />
+            ) : (
+                <>
+                    <div className={styles.home_products}>
+                        {
+                            products && products.map(product => (
+                                <Link to={`/product/${product.slug}`} key={product.id}>
+                                    <div>
+                                        <img src={`${import.meta.env.VITE_API_URL}/images/${product.image}`} alt="Foto Produto" />
+                                        <p>{product.name}</p>
+                                        <p>R${product.price}</p>
+                                        <Link to={`/product/${product.slug}`}><MdReadMore /></Link>
+                                    </div>
+                                </Link>
+                            ))
+                        }
+                    </div>
+                </>
+            )}
         </div>
     )
 }
