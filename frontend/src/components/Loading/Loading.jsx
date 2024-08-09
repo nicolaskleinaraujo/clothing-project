708c52b6a24da1c8451d8f90f6215ff23af6e77c
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-09 11:10:50 -0300

    creates the loading icon

--- frontend/src/components/Loading/Loading.jsx
+++ frontend/src/components/Loading/Loading.jsx
@@ -1,9 +1,18 @@
+// CSS
 import styles from "./Loading.module.css"
+
+// Modules
+import 'ldrs/hatch'
 
 const Loading = () => {
     return (
         <div>
-            loading
+            <l-hatch
+                size="28"
+                stroke="4"
+                speed="3.5"
+                color="black" 
+            ></l-hatch>
         </div>
     )
 }
