23e5da70458d823983bb3f563628e39d44698e34
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-09 11:50:06 -0300

    adds the loading to the createOrder

--- frontend/src/pages/CreateOrder/CreateOrder.jsx
+++ frontend/src/pages/CreateOrder/CreateOrder.jsx
@@ -3,14 +3,17 @@ import styles from "./CreateOrder.module.css"
 
 // Modules
 import dbFetch from "../../config/axios"
+import Loading from "../../components/Loading/Loading"
 import { toast } from "react-toastify"
 import { useState, useEffect, useContext } from "react"
 import { useNavigate } from "react-router-dom"
 import { UserContext } from "../../context/UserContext"
+import { LoadingContext } from "../../context/LoadingContext"
 
 const CreateOrder = () => {
     const navigate = useNavigate()
     const { userId } = useContext(UserContext)
+    const { loading, setLoading } = useContext(LoadingContext)
 
     const [userAddress, setUserAddress] = useState({})
     const [coupon, setCoupon] = useState("")
@@ -24,6 +27,7 @@ const CreateOrder = () => {
             })
 
             setUserAddress(res.data.address)
+            setLoading(false)
         } catch (error) {
             toast.error("Cadastre seu endereço")
             navigate("/address")
@@ -31,33 +35,40 @@ const CreateOrder = () => {
     }
 
     useEffect(() => {
+        setLoading(true)
         getAddress()
     }, [])
 
     return (
         <div>
-            <h2>Endereço</h2>
-            <p>{userAddress.city}, {userAddress.district}, {userAddress.street}, {userAddress.houseNum}</p>
+            { loading ? (
+                <Loading />
+            ) : (
+                <>
+                    <h2>Endereço</h2>
+                    <p>{userAddress.city}, {userAddress.district}, {userAddress.street}, {userAddress.houseNum}</p>
 
-            <h2>Cupom</h2>
-            <div>
-                <input
-                    type="text"
-                    name="coupon"
-                    id="coupon"
-                    placeholder="Digite o cupom"
-                />
-                <button>Adicionar Cupom</button>
-            </div>
+                    <h2>Cupom</h2>
+                    <div>
+                        <input
+                            type="text"
+                            name="coupon"
+                            id="coupon"
+                            placeholder="Digite o cupom"
+                        />
+                        <button>Adicionar Cupom</button>
+                    </div>
 
-            <h2>Entrega</h2>
-            <select name="delivery" id="delivery">
-                <option value="">PAC</option>
-                <option value="">Sedex</option>
-            </select>
+                    <h2>Entrega</h2>
+                    <select name="delivery" id="delivery">
+                        <option value="">PAC</option>
+                        <option value="">Sedex</option>
+                    </select>
 
-            <h2>Detalhes</h2>
-            <p>...</p>
+                    <h2>Detalhes</h2>
+                    <p>...</p>
+                </>
+            )}
         </div>
     )
 }
