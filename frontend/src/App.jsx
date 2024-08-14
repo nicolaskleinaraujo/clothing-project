784c5ee2869ab2d912d86b3dd1e2ef6657989bec
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-14 18:09:05 -0300

    adds tryAuth function to app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,24 @@ import Router from './utils/Router'
 
 // Modules
 import { Flip, ToastContainer } from 'react-toastify'
+import { UserContext } from './context/UserContext'
+import { useContext, useEffect } from 'react'
+import dbFetch from './config/axios'
 
 function App() {
+  const { setUserId, setIsAdmin } = useContext(UserContext)
+
+  const tryAuth = async() => {
+    const res = await dbFetch.post("/users/tryauth")
+
+    setUserId(res.data.user.id)
+    setIsAdmin(res.data.user.isAdmin)
+  }
+
+  useEffect(() => {
+    tryAuth()
+  }, [])
+
   return (
     <>
       <ToastContainer 
