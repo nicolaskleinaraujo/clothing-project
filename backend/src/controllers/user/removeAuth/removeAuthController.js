925cae0a3dd51fefb1908b8d5ceafe5eb82ddf00
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-14 18:22:28 -0300

    finalizes the removeAuthController

--- backend/src/controllers/user/removeAuth/removeAuthController.js
+++ backend/src/controllers/user/removeAuth/removeAuthController.js
@@ -1,7 +1,12 @@
-const prisma = require("../../../db/client")
-
 const removeAuthController = async(req, res) => {
-    
+    try {
+        res.clearCookie("access")
+        res.clearCookie("refresh")
+
+        res.status(200).json({ msg: "Usuario saiu com sucesso" })
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente" })
+    }
 }
 
 module.exports = removeAuthController
