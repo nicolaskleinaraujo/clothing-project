65d7a7c59028c3318cc52e50f3ba3769ba405e4d
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-17 18:54:30 -0300

    creates the filterProducts function

--- frontend/src/pages/SortProducts/SortProducts.jsx
+++ frontend/src/pages/SortProducts/SortProducts.jsx
@@ -1,7 +1,23 @@
 // CSS
 import styles from "./SortProducts.module.css"
 
+// Modules
+import dbFetch from "../../config/axios"
+import { useParams } from "react-router-dom"
+import { useEffect } from "react"
+
 const SortProducts = () => {
+    const { filter } = useParams()
+
+    const filterProducts = async() => {
+        const res = await dbFetch.get(`/products?${filter}`)
+        console.log(res.data)
+    }
+
+    useEffect(() => {
+        filterProducts()
+    }, [])
+
     return (
         <div>SortProducts</div>
     )
