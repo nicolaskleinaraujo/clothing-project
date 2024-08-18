fb8b637c448b67cb446a1cbf78a401b6331c549d
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-18 16:50:28 -0300

    creates the googleRegister page

---
+++ frontend/src/pages/GoogleRegister/GoogleRegister.jsx
@@ -0,0 +1,12 @@
+// CSS
+import styles from "./GoogleRegister.module.css"
+
+const GoogleRegister = () => {
+    return (
+        <div>
+            GoogleRegister
+        </div>
+    )
+}
+
+export default GoogleRegister
