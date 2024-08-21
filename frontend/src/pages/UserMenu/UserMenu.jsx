c0fb1850564761fd86808cfd20fa75cd70a1496f
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-21 16:06:12 -0300

    adds the basic structure to userMenu

--- frontend/src/pages/UserMenu/UserMenu.jsx
+++ frontend/src/pages/UserMenu/UserMenu.jsx
@@ -1,9 +1,26 @@
 // CSS
 import styles from "./UserMenu.module.css"
 
+// Modules
+import dbFetch from "../../config/axios"
+import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { UserContext } from "../../context/UserContext"
+
 const UserMenu = () => {
+    const { userId } = useContext(UserContext)
+
+    const removeAuth = async() => {
+        console.log("removed!")
+    }
+
     return (
-        <div>UserMenu</div>
+        <div>
+            <Link>Minha conta</Link>
+            <Link>Endereços</Link>
+            <Link to={`/orders/${userId}`}>Meus pedidos</Link>
+            <button onClick={removeAuth}>Sair</button>
+        </div>
     )
 }
 
