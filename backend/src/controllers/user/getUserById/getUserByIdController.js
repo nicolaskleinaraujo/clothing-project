34c9e57d00564252bdad9b3dbd7638011bd64812
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-31 16:16:51 -0300

    finalizes the getUserByIdController

--- backend/src/controllers/user/getUserById/getUserByIdController.js
+++ backend/src/controllers/user/getUserById/getUserByIdController.js
@@ -9,7 +9,23 @@ const getUserByIdController = async(req, res) => {
     }
 
     try {
-        
+        const user = await prisma.user.findUnique({ where: { id } })
+
+        if (!user) {
+            res.status(404).json({ msg: "Usuario não encontrado" })
+            return
+        }
+
+        // Checks if the request has been send by the user
+        if (user.id !== parseInt(userId)) {
+            res.status(401).json({ msg: "Usuario não autorizado" })
+            return
+        }
+
+        // Removes user password from the payload
+        delete user.password
+
+        res.status(200).json({ msg: "Usuario encontrado", user })
     } catch (error) {
         res.status(500).json({ msg: "Erro interno, tente novamente", error })
     }
