c3d58fd5b3ede3b6fdb8f20a05cb863cc21812a7
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-08-31 16:30:21 -0300

    creates the basic structure for account page

--- frontend/src/pages/Account/Account.jsx
+++ frontend/src/pages/Account/Account.jsx
@@ -11,7 +11,11 @@ const Account = () => {
     const { userId, setUserId, setIsAdmin } = useContext(UserContext)
 
     const [loading, setLoading] = useState(true)
-    const [userInfos, setUserInfos] = useState({})
+
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [newPassword, setNewPassword] = useState("")
 
     const getUserInfos = async() => {
         setLoading(true)
@@ -21,7 +25,10 @@ const Account = () => {
             "userId": userId,
         })
 
-        setUserInfos(res.data.user)
+        setFirstName(res.data.user.firstName)
+        setLastName(res.data.user.lastName)
+        setEmail(res.data.user.email)
+
         setLoading(false)
     }
 
@@ -35,7 +42,29 @@ const Account = () => {
                 <Loading />
             ) : (
                 <>
-                    loaded
+                    <label>
+                        <p>Nome</p>
+                        <input type="text" onChange={(e) => setFirstName(e.target.value)} value={firstName} />
+                    </label>
+
+                    <label>
+                        <p>Sobrenome</p>
+                        <input type="text" onChange={(e) => setLastName(e.target.value)} value={lastName} />
+                    </label>
+
+                    <label>
+                        <p>Email</p>
+                        <input type="text" onChange={(e) => setEmail(e.target.value)} value={email} />
+                    </label>
+
+                    <label>
+                        <p>Nova senha</p>
+                        <input type="text" onChange={(e) => setNewPassword(e.target.value)} value={newPassword} />
+                    </label>
+
+                    <button>Atualizar Informações</button>
+
+                    <button>Deletar Conta</button>
                 </>
             )}
         </div>
