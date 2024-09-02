50c2abac816c440d77b8fa50168e7253b60740d6
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-02 20:21:53 -0300

    creates the getCoupons function

--- frontend/src/pages/Admin/Coupons/Coupons.jsx
+++ frontend/src/pages/Admin/Coupons/Coupons.jsx
@@ -1,9 +1,34 @@
 // CSS
 import styles from "./Coupons.module.css"
 
+// Modules
+import dbFetch from "../../../config/axios"
+import { useContext, useEffect, useState } from "react"
+import { UserContext } from "../../../context/UserContext"
+
 const Coupons = () => {
+    const { userId } = useContext(UserContext)
+    const [loading, setLoading] = useState(true)
+
+    const [coupons, setCoupons] = useState([])
+
+    const getCoupons = async() => {
+        if (!loading) { setLoading(true) }
+
+        const res = await dbFetch.post("/coupons/get", { userId })
+        setCoupons(res.data.coupons)
+
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        getCoupons()
+    }, [loading])
+
     return (
-        <div>Coupons</div>
+        <div>
+            Coupons
+        </div>
     )
 }
 
