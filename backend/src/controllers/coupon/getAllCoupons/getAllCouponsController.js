1887df5450c8cfd04f54bd1565c3860bfa8f109b
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-02 20:14:27 -0300

    finalizes the getAllCouponsController

--- backend/src/controllers/coupon/getAllCoupons/getAllCouponsController.js
+++ backend/src/controllers/coupon/getAllCoupons/getAllCouponsController.js
@@ -1,7 +1,13 @@
 const prisma = require("../../../db/client")
 
 const getAllCouponsController = async(req, res) => {
-    
+    try {
+        const coupons = await prisma.coupon.findMany()
+
+        res.status(200).json({ msg: "Busca feita com sucesso", coupons })
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente" })
+    }
 }
 
 module.exports = getAllCouponsController
