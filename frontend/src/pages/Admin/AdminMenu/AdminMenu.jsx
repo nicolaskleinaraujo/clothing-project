47178a00f68999346a62ae4dd4e77f7551a8dbbf
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-02 19:44:58 -0300

    creates the basic structure on adminMenu page

--- frontend/src/pages/Admin/AdminMenu/AdminMenu.jsx
+++ frontend/src/pages/Admin/AdminMenu/AdminMenu.jsx
@@ -1,9 +1,35 @@
 // CSS
 import styles from "./AdminMenu.module.css"
 
+// Modules
+import { useState } from "react"
+import { IoIosArrowDown } from "react-icons/io"
+import { Link } from "react-router-dom"
+
 const AdminMenu = () => {
+    const [productsOpen, setProductsOpen] = useState(false)
+    const [categoriesOpen, setCategoriesOpen] = useState(false)
+    const [ordersOpen, setOrdersOpen] = useState(false)
+    const [couponsOpen, setCouponsOpen] = useState(false)
+
     return (
-        <div>AdminMenu</div>
+        <div>
+            <div 
+                onMouseEnter={() => setProductsOpen(true)} 
+                onMouseLeave={() => setProductsOpen(false)} 
+                onClick={() => setProductsOpen(!productsOpen)} 
+            >
+                <button>Produtos <IoIosArrowDown /></button>
+                <div>
+                    <Link to={"/"}>Criar Produto</Link>
+                    <Link to={"/"}>Atualizar Produto</Link>
+                </div>
+            </div>
+
+            <Link to={"/"}>Categorias</Link>
+            <Link to={"/"}>Pedidos</Link>
+            <Link to={"/"}>Cupons</Link>
+        </div>
     )
 }
 
