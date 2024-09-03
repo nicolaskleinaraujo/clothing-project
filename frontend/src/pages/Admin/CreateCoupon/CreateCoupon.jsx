b03d82b7a6104883002932cd910ca7ec18dbcbf6
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-03 11:44:06 -0300

    creates the basic structure on the createCoupon page

--- frontend/src/pages/Admin/CreateCoupon/CreateCoupon.jsx
+++ frontend/src/pages/Admin/CreateCoupon/CreateCoupon.jsx
@@ -7,19 +7,71 @@ import { useState, useContext } from "react"
 import { toast } from "react-toastify"
 import { UserContext } from "../../../context/UserContext"
 import { LoadingContext } from "../../../context/LoadingContext"
+import Loading from "../../../components/Loading/Loading"
 
 const CreateCoupon = () => {
     const { userId } = useContext(UserContext)
     const { loading, setLoading } = useContext(LoadingContext)
 
     const [code, setCode] = useState("")
-    const [percentage, setPercentage] = useState(false)
+    const [percentage, setPercentage] = useState(true)
     const [quantity, setQuantity] = useState(0)
     const [minimum, setMinimum] = useState(0)
 
+    const createCoupon = async() => {
+        console.log("created")
+    }
+
     return (
         <div>
-            CreateCoupon
+            { loading ? (
+                <Loading />
+            ) : (
+                <form onSubmit={createCoupon}>
+                    <h1>Criar cupom</h1>
+
+                    <label>
+                        <p>Código</p>
+                        <input type="text" onChange={(e) => setCode(e.target.value)} value={code} />
+                    </label>
+
+                    <div>
+                        <label>
+                            <input
+                                type="radio"
+                                name="percentage"
+                                id="percentage"
+                                value={true}
+                                onChange={(e) => setPercentage(e.target.value)}
+                            />
+
+                            <p>{"%"}</p>
+                        </label>
+
+                        <label>
+                            <input
+                                type="radio"
+                                name="percentage"
+                                id="percentage"
+                                value={false}
+                                onChange={(e) => setPercentage(e.target.value)}
+                            />
+
+                            <p>{"R$"}</p>
+                        </label>
+                    </div>
+
+                    <label>
+                        <p>Quantidade {percentage ? "%" : "R$"}</p>
+                        <input type="number" min={0} onChange={(e) => setQuantity(e.target.value)} value={quantity} />
+                    </label>
+
+                    <label>
+                        <p>Pedido Minimo R$</p>
+                        <input type="number" min={0} onChange={(e) => setMinimum(e.target.value)} value={minimum} />
+                    </label>
+                </form>
+            )}
         </div>
     )
 }
