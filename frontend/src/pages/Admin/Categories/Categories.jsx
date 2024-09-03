3e875f32eeb0b4874eccaf20ee4eb0a222235add
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-02 22:54:29 -0300

    adds the basic structure on the categories page

--- frontend/src/pages/Admin/Categories/Categories.jsx
+++ frontend/src/pages/Admin/Categories/Categories.jsx
@@ -3,14 +3,30 @@ import styles from "./Categories.module.css"
 
 // Modules
 import dbFetch from "../../../config/axios"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
+import { useNavigate } from "react-router-dom"
+import { LoadingContext } from "../../../context/LoadingContext"
+import { toast } from "react-toastify"
+import Loading from "../../../components/Loading/Loading"
 
 const Categories = () => {
+    const navigate = useNavigate()
+    const { loading, setLoading } = useContext(LoadingContext)
+
     const [categories, setCategories] = useState([])
 
     const getCategories = async() => {
-        const res = await dbFetch.get("/categories")
-        setCategories(res.data.categories)
+        setLoading(true)
+
+        try {
+            const res = await dbFetch.get("/categories")
+            setCategories(res.data.categories)
+        } catch (error) {
+            toast.error("Erro, tente novamente")
+            navigate("/")
+        }
+
+        setLoading(false)
     }
 
     useEffect(() => {
@@ -18,7 +34,33 @@ const Categories = () => {
     }, [])
 
     return (
-        <div>Categories</div>
+        <div>
+            { loading ? (
+                <Loading />
+            ) : (
+                <>
+                    <div className={styles.categories}>
+                        { 
+                            categories.map(category => (
+                                <div key={category.id}>
+                                    <p>{category.name}</p>
+
+                                    <div>
+                                        <button 
+                                            onClick={() => console.log("updated")}
+                                        >Atualizar</button>
+
+                                        <button 
+                                            onClick={() => console.log("deleted")}
+                                        >Excluir</button>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </>
+            )}
+        </div>
     )
 }
 
