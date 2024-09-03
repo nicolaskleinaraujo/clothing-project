60755331191c12e6fc1234f34c795b95b69e2f7d
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-03 11:48:38 -0300

    creates the createCoupon function

--- frontend/src/pages/Admin/CreateCoupon/CreateCoupon.jsx
+++ frontend/src/pages/Admin/CreateCoupon/CreateCoupon.jsx
@@ -4,12 +4,14 @@ import styles from "./CreateCoupon.module.css"
 // Modules
 import dbFetch from "../../../config/axios"
 import { useState, useContext } from "react"
+import { useNavigate } from "react-router-dom"
 import { toast } from "react-toastify"
 import { UserContext } from "../../../context/UserContext"
 import { LoadingContext } from "../../../context/LoadingContext"
 import Loading from "../../../components/Loading/Loading"
 
 const CreateCoupon = () => {
+    const navigate = useNavigate()
     const { userId } = useContext(UserContext)
     const { loading, setLoading } = useContext(LoadingContext)
 
@@ -18,8 +20,24 @@ const CreateCoupon = () => {
     const [quantity, setQuantity] = useState(0)
     const [minimum, setMinimum] = useState(0)
 
-    const createCoupon = async() => {
-        console.log("created")
+    const createCoupon = async(e) => {
+        e.preventDefault()
+        setLoading(true)
+
+        try {
+            await dbFetch.post("/coupons", {
+                code,
+                percentage,
+                quantity: parseFloat(quantity),
+                minimum: parseFloat(minimum),
+                userId,
+            })
+
+            navigate("/admin/coupons")
+        } catch (error) {
+            toast.error("Erro, tente novamente")
+            setLoading(false)
+        }
     }
 
     return (
@@ -70,6 +88,8 @@ const CreateCoupon = () => {
                         <p>Pedido Minimo R$</p>
                         <input type="number" min={0} onChange={(e) => setMinimum(e.target.value)} value={minimum} />
                     </label>
+
+                    <input type="submit" value="Criar" />
                 </form>
             )}
         </div>
