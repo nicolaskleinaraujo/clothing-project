ac2771febf92f4b7d6e30bad31eb5c622ff2bcd8
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-04 11:18:25 -0300

    finishes the adminSortController

--- backend/src/controllers/order/adminSort/adminSortController.js
+++ backend/src/controllers/order/adminSort/adminSortController.js
@@ -3,15 +3,21 @@ const prisma = require("../../../db/client")
 const adminSortController = async(req, res) => {
     const { paid, received } = req.query
 
-    // Creating the where payload used to sort the products
-    let orders 
+    const formatedPaid = JSON.parse(paid)
+    const formatedReceived = JSON.parse(received)
 
     try {
-        if (paid !== "" && received === "") {
-            orders = await prisma.orders.findMany({ where: { paid } })
-        } else if (paid === "" && received !== "") {
-            orders = await prisma.orders.findMany({ where: { received } })
+        let orders = {}
+
+        if (formatedPaid === true && formatedReceived === false) {
+            orders = await prisma.orders.findMany({ where: { paid: formatedPaid } })
+        } else if (formatedPaid === false && formatedReceived === false) {
+            orders = await prisma.orders.findMany({ where: { received: formatedReceived } })
+        } else if (formatedPaid === false && received === false) {
+            orders = await prisma.orders.findMany()
         }
+
+        res.status(200).json({ msg: "Busca feita com sucesso", orders })
     } catch (error) {
         res.status(500).json({ msg: "Erro interno, tente novamente" })
     }
