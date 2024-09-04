649d37997433f991bf48ea9cae1e78da5cfd7f08
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-04 11:28:22 -0300

    creates the getAllOrders function

--- frontend/src/pages/Admin/AllOrders/AllOrders.jsx
+++ frontend/src/pages/Admin/AllOrders/AllOrders.jsx
@@ -3,14 +3,56 @@ import styles from "./AllOrders.module.css"
 
 // Modules
 import dbFetch from "../../../config/axios"
+import { useContext, useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { LoadingContext } from "../../../context/LoadingContext"
+import { UserContext } from "../../../context/UserContext"
+import { toast } from "react-toastify"
+import Loading from "../../../components/Loading/Loading"
 
 const AllOrders = () => {
+    const navigate = useNavigate()
+    const { userId } = useContext(UserContext)
+    const { loading, setLoading } = useContext(LoadingContext)
+
+    const [paid, setPaid] = useState(false)
+    const [received, setReceived] = useState(false)
+
+    const [orders, setOrders] = useState([])
+
     const getAllOrders = async() => {
-        console.log("gotten")
+        setLoading(true)
+
+        try {
+            const res = await dbFetch.post(`/orders/sort?paid=${paid}&received=${received}`, { userId })
+
+            setOrders(res.data.orders)
+
+            setLoading(false)
+        } catch (error) {
+            toast.error("Erro, tente novamente")
+            navigate("/admin")
+        }
     }
 
+    useEffect(() => {
+        getAllOrders()
+    }, [])
+
     return (
-        <div>AllOrders</div>
+        <div>
+            { loading ? (
+                <Loading />
+            ) : (
+                <>
+                    {orders.map(order => (
+                        <div>
+                            {order.price}
+                        </div>
+                    ))}
+                </>
+            )}
+        </div>
     )
 }
 
