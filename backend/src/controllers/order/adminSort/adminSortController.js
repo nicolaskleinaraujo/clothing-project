860ec13c0982708f2a86957afedbc6b55dbde04a
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-04 11:09:47 -0300

    adds all validations to adminSortController

--- backend/src/controllers/order/adminSort/adminSortController.js
+++ backend/src/controllers/order/adminSort/adminSortController.js
@@ -1,7 +1,20 @@
 const prisma = require("../../../db/client")
 
 const adminSortController = async(req, res) => {
-    
+    const { paid, received } = req.query
+
+    // Creating the where payload used to sort the products
+    let orders 
+
+    try {
+        if (paid !== "" && received === "") {
+            orders = await prisma.orders.findMany({ where: { paid } })
+        } else if (paid === "" && received !== "") {
+            orders = await prisma.orders.findMany({ where: { received } })
+        }
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente" })
+    }
 }
 
 module.exports = adminSortController
