80fa24ceec63b09534359f245c3ab5518ce5e8f8
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-05 22:00:22 -0300

    creates the getProducts function

--- frontend/src/pages/Admin/Products/Products.jsx
+++ frontend/src/pages/Admin/Products/Products.jsx
@@ -1,9 +1,39 @@
 // CSS
 import styles from "./Products.module.css"
 
+// Modules
+import dbFetch from "../../../config/axios"
+import { useState, useEffect, useContext } from "react"
+import { useNavigate } from "react-router-dom"
+import { LoadingContext } from "../../../context/LoadingContext"
+import { toast } from "react-toastify"
+
 const Products = () => {
+    const navigate = useNavigate()
+    const { loading, setLoading } = useContext(LoadingContext)
+
+    const [products, setProducts] = useState([])
+
+    const getProducts = async() => {
+        setLoading(true)
+
+        try {
+            const res = await dbFetch.get("/products")
+
+            setProducts(res.data.products)
+
+            setLoading(false)
+        } catch (error) {
+            toast.error(error.response.data.msg)
+            setLoading(false)
+            navigate("/admin")
+        }
+    }
+
     return (
-        <div>Products</div>
+        <div>
+
+        </div>
     )
 }
 
