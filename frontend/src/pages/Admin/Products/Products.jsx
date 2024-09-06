587158137efe69d51255f01b9ece2bfbb9ab5f31
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-06 20:48:32 -0300

    creates the css to the products page

--- frontend/src/pages/Admin/Products/Products.jsx
+++ frontend/src/pages/Admin/Products/Products.jsx
@@ -131,6 +131,8 @@ const Products = () => {
                             value={quantity} 
                         />
                     </label>
+
+                    <input type="submit" value={slug === undefined ? "Criar" : "Atualizar"} />
                 </form>
             )}
         </div>
