635f7ea1454c2bbe9e43a9baca034b8481685ca6
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-06 20:47:12 -0300

    creates the basic structure on the products page

--- frontend/src/pages/Admin/Products/Products.jsx
+++ frontend/src/pages/Admin/Products/Products.jsx
@@ -45,12 +45,95 @@ const Products = () => {
         }
     }
 
+    const handlePrice = (e) => {
+        console.log(e.target.value)
+    }
+
     useEffect(() => {
         getProduct()
     }, [])
 
     return (
-        <div>EditProducts</div>
+        <div>
+            { loading ? (
+                <Loading />
+            ) : (
+                <form onSubmit={console.log("Submited")} className={styles.products}>
+                    { slug === undefined ? (
+                        <h1>Novo Produto</h1>
+                    ) : (
+                        <h1>Atualizar Produto</h1>
+                    )}
+
+                    <label>
+                        <p>Nome</p>
+                        <input 
+                            type="text" 
+                            name="name" 
+                            id="name" 
+                            value={name} 
+                            required
+                            onChange={(e) => setName(e.target.value)} 
+                        />
+                    </label>
+
+                    <label>
+                        <p>Descrição</p>
+                        <textarea 
+                            name="description" 
+                            id="description" 
+                            value={description} 
+                            onChange={(e) => setDescription(e.target.value)}
+                        ></textarea>
+                    </label>
+
+                    <label>
+                        <p>Preço</p>
+                        <input 
+                            type="text" 
+                            name="price" 
+                            id="price" 
+                            onChange={(e) => handlePrice(e)} 
+                            value={price} 
+                        />
+                    </label>
+
+                    <label>
+                        <p>Tamanhos</p>
+                        <input 
+                            type="text" 
+                            name="sizes" 
+                            id="sizes" 
+                            onChange={(e) => setSizes(e.target.value)} 
+                            value={sizes} 
+                            placeholder="Preto, Azul"
+                        />
+                    </label>
+
+                    <label>
+                        <p>Cores</p>
+                        <input 
+                            type="text" 
+                            name="colors" 
+                            id="colors" 
+                            onChange={(e) => setColors(e.target.value)} 
+                            value={colors} 
+                        />
+                    </label>
+
+                    <label>
+                        <p>Quantidade</p>
+                        <input 
+                            type="text" 
+                            name="quantity" 
+                            id="quantity" 
+                            onChange={(e) => setQuantity(e.target.value)}  
+                            value={quantity} 
+                        />
+                    </label>
+                </form>
+            )}
+        </div>
     )
 }
 
