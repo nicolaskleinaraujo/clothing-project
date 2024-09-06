06dc62b6d7562bb3f4cb8e282b4f42abb8a95eee
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-05 22:12:33 -0300

    adds product rendering to the products page

--- frontend/src/pages/Admin/Products/Products.jsx
+++ frontend/src/pages/Admin/Products/Products.jsx
@@ -7,6 +7,7 @@ import { useState, useEffect, useContext } from "react"
 import { useNavigate } from "react-router-dom"
 import { LoadingContext } from "../../../context/LoadingContext"
 import { toast } from "react-toastify"
+import Loading from "../../../components/Loading/Loading"
 
 const Products = () => {
     const navigate = useNavigate()
@@ -30,9 +31,32 @@ const Products = () => {
         }
     }
 
+    useEffect(() => {
+        getProducts()
+    }, [])
+
     return (
         <div>
-
+            { loading ? (
+                <Loading />
+            ) : (
+                <>
+                    <div className={styles.products_products}>
+                        {
+                            products.map(product => (
+                                <div key={product.id}>
+                                    <img src={`${import.meta.env.VITE_API_URL}/images/${product.image.split(", ")[0]}`} alt="Foto Produto" />
+                                    <p>{product.name}</p>
+                                    <p>R${product.price.toFixed(2)}</p>
+                                    <p>Tamanhos: {product.sizes.map(size => size.size + ", ")}</p>
+                                    <p>Cores: {product.colors}</p>
+                                    <p>Estoque: {product.quantity}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </>
+            )}
         </div>
     )
 }
