32d8d3a68a87939a987e532fb26fe938071beda8
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-07 11:11:26 -0300

    hotfixes the multer config file

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -1,17 +1,6 @@
 const multer = require("multer")
-const path = require("node:path")
 
-const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, path.resolve("product_images"))
-    },
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
 
-    filename: (req, file, callback) => {
-        const time = new Date().getTime()
-        req.imagename = `${time}_${file.originalname}`
-
-        callback(null, `${time}_${file.originalname}`)
-    }
-})
-
-module.exports = storage
+module.exports = upload
