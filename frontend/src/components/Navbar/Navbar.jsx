e22a51e8a653c4950affb154100934d5a8539387
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-09 19:34:59 -0300

    creates the new structure on the navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import dbFetch from "../../config/axios"
 
 // Modules
 import { useState, useEffect, useContext } from "react"
-import { FiUser, FiShoppingBag } from "react-icons/fi"
+import { FiUser, FiShoppingBag, FiMenu } from "react-icons/fi"
 import { IoIosArrowDown } from "react-icons/io"
 import { UserContext } from "../../context/UserContext"
 import { Link } from "react-router-dom"
@@ -32,39 +32,11 @@ const Navbar = () => {
 
     return (
         <div className={styles.navbar}>
-            <Link to="/"><img src="/project-logo.png" alt="Logo do Projeto" /></Link>
-
-            <div 
-                className={styles.navbar_category} 
-                onMouseEnter={() => setCategoriesOpen(true)} 
-                onMouseLeave={() => setCategoriesOpen(false)} 
-                onClick={() => setCategoriesOpen(!categoriesOpen)} 
-            >
-                <button>Categorias <IoIosArrowDown /></button>
-                <div className={categoriesOpen ? styles.navbar_category_open_content : styles.navbar_category_content}>
-                    {
-                        categories && categories.map(category => (
-                            <Link to={`/sort-products/category=${category.id}`} key={category.id}>{category.name}</Link>
-                        ))
-                    }
-                </div>
+            <div className={styles.navbar_menu}>
+                <button><FiMenu /></button>
             </div>
 
-            <div 
-                className={styles.navbar_size} 
-                onMouseEnter={() => setSizesOpen(true)} 
-                onMouseLeave={() => setSizesOpen(false)} 
-                onClick={() => setSizesOpen(!sizesOpen)} 
-            >
-                <button>Tamanhos <IoIosArrowDown /></button>
-                <div className={sizesOpen ? styles.navbar_size_open_content : styles.navbar_size_content}>
-                    {
-                        sizes && sizes.map(size => (
-                            <Link to={`/sort-products/sizes=${size.size}`} key={size.id}>{size.size}</Link>
-                        ))
-                    }
-                </div>
-            </div>
+            <Link to="/"><img src="/project-big-logo.png" alt="Logo do Projeto" /></Link>
 
             <div className={styles.navbar_account}>
                 <Link to={userId === 0 ? "/login" : "/menu"}><FiUser /></Link>
