8225ec8e9619f30441ee4f1914910ea7d1e3c0ce
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-17 20:54:22 -0300

    adds the pagination system to the sortProducts

--- frontend/src/pages/SortProducts/SortProducts.jsx
+++ frontend/src/pages/SortProducts/SortProducts.jsx
@@ -3,7 +3,7 @@ import styles from "./SortProducts.module.css"
 
 // Modules
 import dbFetch from "../../config/axios"
-import { MdReadMore } from "react-icons/md"
+import { MdFirstPage, MdLastPage, MdReadMore } from "react-icons/md"
 import { useParams, Link, useNavigate } from "react-router-dom"
 import { useEffect, useContext, useState } from "react"
 import { LoadingContext } from "../../context/LoadingContext"
@@ -17,27 +17,39 @@ const SortProducts = () => {
     const { loading, setLoading } = useContext(LoadingContext)
 
     const [products, setProducts] = useState([])
+    const [pages, setPages] = useState(0)
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const [startPage, setStartPage] = useState(0)
+    const [endPage, setEndPage] = useState(0)
 
     const filterProducts = async() => {
         setLoading(true)
 
         try {
-            const res = await dbFetch.get(`/products?${filter}`)
+            const res = await dbFetch.get(`/products?${filter}&page=${currentPage}`)
             setProducts(res.data.products)
+            setPages(res.data.totalPages)
 
-            if (res.data.product.length === 0) {
+            // Calculates the pages to be shown on the pagination system
+            setStartPage(Math.max(1, currentPage - 1))
+            setEndPage(Math.min(res.data.totalPages, currentPage + 1))
+
+            if (res.data.products.length === 0) {
                 toast.info("Nenhum produto com o filtro selecionado")
-                navigate("/")
+                return navigate("/")
             }
         } catch (error) {
             toast.info("Nenhum produto com o filtro selecionado")
-            navigate("/")
+            return navigate("/")
         }
+
+        setLoading(false)
     }
 
     useEffect(() => {
         filterProducts()
-    }, [filter])
+    }, [filter, currentPage])
 
     return (
         <div>
@@ -58,6 +70,22 @@ const SortProducts = () => {
                                 </Link>
                             ))
                         }
+                    </div>
+
+                    <div className={styles.home_pagination}>
+                        { currentPage > 1 && <button onClick={() => setCurrentPage(1)}><MdFirstPage /></button> }
+
+                        { currentPage > 2 && <button style={{ cursor: "default" }}>...</button> }
+
+                        { pages > 0 && 
+                            Array.from({ length: pages }, (_, index) => index + 1).slice(startPage - 1, endPage).map(page => (
+                                <button key={page} onClick={() => setCurrentPage(page)}>{page}</button>
+                            ))
+                        }
+
+                        { endPage < pages && <button style={{ cursor: "default" }}>...</button> }
+
+                        { currentPage < pages && <button onClick={() => setCurrentPage(pages)}><MdLastPage /></button> }
                     </div>
                 </>
             )}
