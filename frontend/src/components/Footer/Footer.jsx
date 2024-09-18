1a915e9397cd39fdbe1ab077a5a745e82461c61c
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-17 22:18:01 -0300

    styles the compliments section

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,27 +1,32 @@
 // CSS
 import styles from "./Footer.module.css"
 
+// Modules
+import { FiTruck, FiThumbsUp } from "react-icons/fi"
+import { MdOutlineMarkChatRead } from "react-icons/md"
+import { AiOutlineSafety } from "react-icons/ai"
+
 const Footer = () => {
     return (
         <div className={styles.footer}>
             <div className={styles.footer_compliments}>
                 <div>
-                    <h2>Entregamos em todo Brasil</h2>
+                    <h2><FiTruck /> Entregamos em todo Brasil</h2>
                     <p>Envio imediato após a confirmação de pagamento</p>
                 </div>
 
                 <div>
-                    <h2>Cliente Satisfeito</h2>
+                    <h2><FiThumbsUp /> Cliente Satisfeito</h2>
                     <p>Nota maxima no <a href="http://www.reclameaqui.com.br" target="_blank">Reclame AQUI</a></p>
                 </div>
 
                 <div>
-                    <h2>Suporte ao cliente</h2>
+                    <h2><MdOutlineMarkChatRead /> Suporte ao cliente</h2>
                     <p>Atendimento de Seg. a Sab. das 8h às 15h</p>
                 </div>
 
                 <div>
-                    <h2>Pagamento seguro</h2>
+                    <h2><AiOutlineSafety /> Pagamento seguro</h2>
                     <p>Pagamento gerenciado pelo Mercado Pago para assegurar sua segurança</p>
                 </div>
             </div>
