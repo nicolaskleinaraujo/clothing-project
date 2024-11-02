9084f3f146f3438e1af5d9590a9af2ccefa624a9
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-02 15:54:38 -0300

    creates the cardPayment component

--- frontend/src/components/CardPayment/CardPayment.jsx
+++ frontend/src/components/CardPayment/CardPayment.jsx
@@ -1,10 +1,30 @@
 // CSS
 import React from 'react'
 
-const CardPayment = () => {
+// Modules
+import { CardPayment } from '@mercadopago/sdk-react'
+
+const CardPayment = ({ amount }) => {
+    const onSubmit = async(formData) => {
+        console.log(formData)
+    }
+
+    const onError = async(error) => {
+        console.log(error);
+    }
+
+    const onReady = async() => {
+        console.log("Ready")
+    }
+
     return (
         <div>
-            
+            <CardPayment
+                initialization={{ amount }}
+                onSubmit={onSubmit}
+                onReady={onReady}
+                onError={onError}
+            />
         </div>
     )
 }
