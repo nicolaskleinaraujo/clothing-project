54aeac8615b865941b719687a4948155362eb434
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-02 16:56:06 -0300

    adds the order update on cardPayment

--- backend/src/controllers/order/cardPayment/cardPayment.js
+++ backend/src/controllers/order/cardPayment/cardPayment.js
@@ -6,6 +6,18 @@ const cardPayment = async(req, res) => {
 
     try {
         const process = await processPayment(req.body.formData)
+
+        if (process.status === "rejected") {
+            res.status(400).json({ msg: "Pagamento rejeitado, tente novamente" })
+            return
+        }
+
+        const order = await prisma.orders.update({
+            where: { id: parseInt(id) },
+            data: { paid: true },
+        })
+
+        res.status(200).json({ msg: "Pagamento efetuado com sucesso", order })
     } catch (error) {
         res.status(500).json({ msg: "Erro interno, tente novamente", error })
     }
