f1d84cbcb806713766bea86b0020ea0743c42060
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-04 11:25:05 -0300

    adds the product cart quantity return

--- backend/src/controllers/cart/calculatePrice/calculatePriceController.js
+++ backend/src/controllers/cart/calculatePrice/calculatePriceController.js
@@ -60,6 +60,7 @@ const calculatePriceController = async(req, res) => {
         // Adds the product to order products
         product.sizes = product.sizes.filter(size => size.id === cart[index].sizeId)
         product.colors = product.colors.split(", ").filter(color => color == cart[index].color)
+        product.quantity = cart[index].quantity
         orderProducts.push(product)
 
         // Calculates the product price
